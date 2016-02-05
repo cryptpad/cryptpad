7102a0a1646e90062dd47c9ad2cff7d1f6370484
ansuz <ansuz@transitiontech.ca>
2016-02-05 11:48:34 +0100

    make rainbows

---
+++ www/common/rainbow.js
@@ -0,0 +1,29 @@
+define([], function () {
+    return function (n) {
+        var r = 0.6,
+            n = n || 24, // default is 24 colours
+            i = 0,
+            t = [],
+            rgb = [0,2,4];
+
+        while(i<n)t.push(i++);
+
+        var colours = t.map(function (c, I) {
+            return '#'+ rgb.map(function (j) {
+                var x = ((Math.sin(r*(I+22)+j)*127+128) *0x01<<0)
+                    .toString(16);
+                return x.length<2?"0"+x:x;
+            }).join("");
+        });
+
+        var J = 0;
+        return function () {
+            var j = J++;
+            if (colours[j]) {
+                return colours[j];
+            }
+            J = 0;
+            return colours[0];
+        };
+    };
+});
