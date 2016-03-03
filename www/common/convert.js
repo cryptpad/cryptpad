7d65540123285addf0e27f3689a15b64f32f9afa
ansuz <ansuz@transitiontech.ca>
2016-03-03 15:12:43 +0100

    fix undefined reference

--- www/common/convert.js
+++ www/common/convert.js
@@ -1,16 +1,14 @@
 define([
-    '/common/virtual-dom.js',
     '/common/hyperjson.js',
     '/common/hyperscript.js'
-], function (vdom, hyperjson, hyperscript) {
+], function (hyperjson, hyperscript) {
     // complain if you don't find the required APIs
-    if (!(vdom && hyperjson && hyperscript)) { throw new Error(); }
-    
+    if (!(hyperjson && hyperscript)) { throw new Error(); }
+
     // Generate a matrix of conversions
     /*
         convert.dom.to.hjson, convert.hjson.to.dom,
         convert.dom.to.vdom, convert.vdom.to.dom,
-        convert.vdom.to.hjson, convert.hjson.to.vdom
 
         and of course, identify functions in case you try to
         convert a datatype to itself
@@ -36,17 +34,8 @@ define([
                     vdom: function (H) {
                         return hyperjson.callOn(H, vdom.h);
                     }
-                },
-                vdom:{
-                    vdom: Self,
-                    dom: function (V) {
-                        return vdom.create(V);
-                    },
-                    hjson: function (V) {
-                        return hyperjson.fromDOM(vdom.create(V));
-                    }
                 }
-            }, 
+            },
             convert = {};
         Object.keys(methods).forEach(function (method) {
             convert[method] = { to: methods[method] };
@@ -55,7 +44,6 @@ define([
     }());
 
     convert.core = {
-        vdom: vdom,
         hyperjson: hyperjson,
         hyperscript: hyperscript
     };
