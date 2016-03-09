c67451bc1a0164a42fc2f5c4c8c4886167293e28
ansuz <ansuz@transitiontech.ca>
2016-03-09 10:29:51 +0100

    Revert "remove broken functions from convert module"
    Because I forgot that the /render/ page was still using vdom
    
    This reverts commit 93fb944e1ff7802f1970bcad3cd3ca149a23cdf3.

--- www/common/convert.js
+++ www/common/convert.js
@@ -20,13 +20,19 @@ define([
             methods = {
                 dom:{
                     dom: Self,
-                    hjson: hyperjson.fromDOM
+                    hjson: hyperjson.fromDOM,
+                    vdom: function (D) {
+                        return hyperjson.callOn(hyperjson.fromDOM(D), vdom.h);
+                    }
                 },
                 hjson:{
                     hjson: Self,
                     dom: function (H) {
                         // hyperjson.fromDOM,
                         return hyperjson.callOn(H, hyperscript);
+                    },
+                    vdom: function (H) {
+                        return hyperjson.callOn(H, vdom.h);
                     }
                 }
             },
