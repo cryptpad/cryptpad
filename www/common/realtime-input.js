38d797a4f13d356eb883b23e108d2b87e0dd30e1
Yann Flory <yann.flory@xwiki.com>
2016-03-16 17:38:26 +0100

    Fix issue with Chrome and WebRTC

--- www/common/realtime-input.js
+++ www/common/realtime-input.js
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+window.Reflect = { has: (x,y) => { return (y in x); } };
 define([
     '/common/messages.js',
     '/common/netflux.js',
