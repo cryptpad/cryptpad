c50690349b4cd40643530deeef5074527d5d41b2
ansuz <ansuz@transitiontech.ca>
2016-04-01 10:46:54 +0200

    comments describing the role of each argument

--- www/common/json-ot.js
+++ www/common/json-ot.js
@@ -6,6 +6,11 @@ define([
 
     var validate = JsonOT.validate = function (text, toTransform, transformBy) {
         try {
+            // text = O (mutual common ancestor)
+            // toTransform = A (the first incoming operation)
+            // transformBy = B (the second incoming operation)
+            // threeway merge (0, A, B)
+
             var resultOp = ChainPad.Operation.transform0(text, toTransform, transformBy);
             var text2 = ChainPad.Operation.apply(transformBy, text);
             var text3 = ChainPad.Operation.apply(resultOp, text2);
