07fdeebe17929e3f1feb74940cf2d401474983a1
ansuz <ansuz@transitiontech.ca>
2016-04-12 14:50:32 +0200

    LogStore.js : JSHint compliance and slight reformat

--- storage/LogStore.js
+++ storage/LogStore.js
@@ -1,20 +1,18 @@
-    var Fs = require("fs");
+var Fs = require("fs");
 
-    var message = function(file, msg) {
-        file.write(msg+"\n");
-    }
+var message = function(file, msg) {
+    file.write(msg+"\n");
+};
 
-    var create = module.exports.create = function(filePath, backingStore) {
+var create = module.exports.create = function(filePath, backingStore) {
 
-        var file = Fs.createWriteStream(filePath, {flags: 'a+'});
+    var file = Fs.createWriteStream(filePath, {flags: 'a+'});
 
-        return {
-            message: function(channel, msg, callback) {
-                message(file, msg);
-                backingStore.message(channel, msg, callback);
-            },
-            getMessages: backingStore.getMessages
-        }
-
-    }
-    
+    return {
+        message: function(channel, msg, callback) {
+            message(file, msg);
+            backingStore.message(channel, msg, callback);
+        },
+        getMessages: backingStore.getMessages
+    };
+};
