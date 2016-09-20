e6785b4dade5010ed18c5c59f2bc2bef1ebc9451
ansuz <ansuz@transitiontech.ca>
2016-09-20 16:35:55 +0200

    push /pad/ state once ready to avoid OT collision

--- www/pad/main.js
+++ www/pad/main.js
@@ -613,6 +613,7 @@ define([
                     console.log("Unlocking editor");
                     setEditable(true);
                     initializing = false;
+                    onLocal();
                 });
             };
 
