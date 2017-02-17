2daa9f8658164499168dc2f30b21619f4b5b592c
yflory <yann.flory@xwiki.com>
2017-02-17 10:57:56 +0100

    Link to main page was opened in the inner iframe

--- www/common/toolbar.js
+++ www/common/toolbar.js
@@ -321,7 +321,17 @@ define([
         var $span = $('<span>').text('CryptPad');
         var $aTagBig = $aTagSmall.clone().addClass('large').append($span);
         $aTagSmall.addClass('narrow');
+        var onClick = function (e) {
+            e.preventDefault();
+            if (e.ctrlKey) {
+                window.open('/');
+                return;
+            }
+            window.location = "/";
+        };
 
+        $aTagBig.click(onClick);
+        $aTagSmall.click(onClick);
         $linkContainer.append($aTagSmall).append($aTagBig);
     };
 
