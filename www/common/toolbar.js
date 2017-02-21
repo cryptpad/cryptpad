0bdf59c1dd9ddaaa22c326659c1657cdc8d9e7e0
yflory <yann.flory@xwiki.com>
2017-02-21 10:42:06 +0100

    Fix broken link in the 'New' button on cryptpad.fr

--- www/common/toolbar.js
+++ www/common/toolbar.js
@@ -362,7 +362,7 @@ define([
                     tag: 'a',
                     attributes: {
                         'target': '_blank',
-                        'href': '/' + p,
+                        'href': '/' + p + '/',
                     },
                     content: Messages.type[p]
                 });
