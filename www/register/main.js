345d2faab2d462fadf39dcf02f3ab553315fa422
yflory <yann.flory@xwiki.com>
2017-02-28 17:13:51 +0100

    Fix horizontal scrollbar in Edge #3

--- www/register/main.js
+++ www/register/main.js
@@ -16,6 +16,8 @@ define([
     $(function () {
         var $main = $('#mainBlock');
 
+        $('#noscriptContainer').remove();
+
         // Language selector
         var $sel = $('#language-selector');
         Cryptpad.createLanguageSelector(undefined, $sel);
