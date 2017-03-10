b3cc8da315c0017ff78ef36136390e020b30bb26
ansuz <ansuz@transitiontech.ca>
2017-03-10 18:03:52 +0100

    implement basic clientside RPC infrastructure
    and provide a basic example for testing it

--- www/common/rpc.js
+++ www/common/rpc.js
@@ -3,26 +3,73 @@ define([
 
     '/bower_components/tweetnacl/nacl-fast.min.js',
 ], function (Encode) {
+    var MAX_LAG_BEFORE_TIMEOUT = 30000;
 
-    var getHistoryKeeperName = function (network) {
-        var wc = network.webChannels[0];
-        if (!wc) {
-            throw new Error("ERROR: no joined webchannels so we can't get the history keeper name");
+    var uid = function () {
+        return Number(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
+            .toString(32).replace(/\./g, '');
+    };
+
+    /*
+types of messages:
+    pin -> hash
+    unpin -> hash
+    getHash -> hash
+    getTotalSize -> bytes
+    getFileSize -> bytes
+
+    */
+
+/*  RPC communicates only with the history keeper
+    messages have the format:
+        [TYPE, txid, msg]
+*/
+    var sendMsg = function (ctx, type, msg, cb) {
+        var network = ctx.network;
+        var hkn = network.historyKeeper;
+        var txid = uid();
+
+        ctx.pending[txid] = cb;
+
+        return network.sendto(hkn, JSON.stringify([txid, type, msg]));
+    };
+
+    var parse = function (msg) {
+        try {
+            return JSON.parse(msg);
+        } catch (e) {
+            return null;
         }
-        if (!wc.history_keeper) { throw new Error("ERROR: no history keeper"); }
-        return wc.history_keeper;
     };
 
-    var sendMsg = function (ctx, cb) {
-        var hkn = getHistoryKeeperName(ctx.network);
-    };
-
+/*  Returning messages have the format:
+    [txid, {}]
+*/
     var onMsg = function (ctx, msg) {
-        console.log(msg);
+        var parsed = parse(msg);
+
+        if (!parsed) {
+            // TODO handle error
+            console.log(msg);
+            return;
+        }
+
+        var txid = parsed[0];
+        var pending = ctx.pending[txid];
+        var response = parsed.slice(1);
+
+        if (typeof(pending) === 'function') {
+            if (response[0] === 'ERROR') {
+                return void pending(response[1]);
+            }
+            pending(void 0, response);
+        } else {
+            console.log("No callback provided");
+        }
     };
 
     var cookie = function (ctx, cb) {
-        // TODO
+        // TODO txid
     };
 
     var signMsg = function (msg, secKey) {
@@ -31,16 +78,27 @@ define([
 
     var create = function (network, edPrivateKey) {
         if (!/[0-9a-f]{64}/.test(edPrivateKey)) {
-            throw new Error("private signing key is not valid");
+            //throw new Error("private signing key is not valid");
         }
         var ctx = {
-            privateKey: Encode.hexToUint8Array(edPrivateKey),
+            //privateKey: Encode.hexToUint8Array(edPrivateKey),
             seq: new Date().getTime(),
-            network: network
+            network: network,
+            timeouts: {}, // timeouts
+            pending: {}, // callbacks
         };
-        network.on('message', function (msg) { onMsg(ctx, msg); });
+
+        var pin = function (channel, cb) { };
+
+        var send = function (type, msg, cb) {
+            return sendMsg(ctx, type, msg, cb);
+        };
+        network.on('message', function (msg, sender) {
+            onMsg(ctx, msg);
+        });
         return {
             cookie: function (cb) { cookie(ctx, cb); },
+            send: send,
         };
     };
 
