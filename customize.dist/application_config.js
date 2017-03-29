6186953a12589ebd05c14b6638dd598837305fcc
ansuz <ansuz@transitiontech.ca>
2017-03-29 11:54:19 +0200

    make whiteboard a default app

--- customize.dist/application_config.js
+++ customize.dist/application_config.js
@@ -4,7 +4,7 @@ define(function() {
     /* Select the buttons displayed on the main page to create new collaborative sessions
      * Existing types : pad, code, poll, slide
      */
-    config.availablePadTypes = ['drive', 'pad', 'code', 'slide', 'poll'];
+    config.availablePadTypes = ['drive', 'pad', 'code', 'slide', 'poll', 'whiteboard'];
 
     /*  Cryptpad apps use a common API to display notifications to users
      *  by default, notifications are hidden after 5 seconds
