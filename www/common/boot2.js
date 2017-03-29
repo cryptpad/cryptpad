d4ce3dc6b9301a6fc642eab5a10917a53811b172
ansuz <ansuz@transitiontech.ca>
2017-03-29 16:02:04 +0200

    don't rewrite css rules. FOUC is too long

--- www/common/boot2.js
+++ www/common/boot2.js
@@ -3,6 +3,5 @@ define([], function () {
     // fix up locations so that relative urls work.
     require.config({ baseUrl: window.location.pathname });
     require([document.querySelector('script[data-bootload]').getAttribute('data-bootload')]);
-    require(['/common/rewrite-css.js']);
 });
 
