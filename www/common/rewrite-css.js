3cfd0a4326f78bf6118c030497708225705a1240
ansuz <ansuz@transitiontech.ca>
2017-03-29 15:40:34 +0200

    commit some garbage

--- www/common/rewrite-css.js
+++ www/common/rewrite-css.js
@@ -1,10 +1,12 @@
 define([
     '/api/config',
 ], function (Config) {
-    if (!(Config && Config.requireConf && Config.requireConf.urlArgs)) { return; }
-
+    var urlArgs = Config && Config.requireConf && Config.requireConf.urlArgs;
+    if (!urlArgs) { return; }
     document.querySelectorAll('link[rel="stylesheet"][data-rewrite-href]').forEach(function (e) {
-        e.setAttribute('href', e.getAttribute('data-rewrite-href'));
+        var href = e.getAttribute('data-rewrite-href');
+		href += (/\?/.test(href)?'&':'?') + urlArgs;
+        e.setAttribute('href', href);
     });
 });
 
