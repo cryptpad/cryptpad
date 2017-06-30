75c2a1fd73efa51c8732899822bd60e9292d0bbc
ansuz <ansuz@transitiontech.ca>
2017-06-30 11:20:56 +0200

    jshint compliance

--- www/common/cryptpad-common.js
+++ www/common/cryptpad-common.js
@@ -1299,7 +1299,7 @@ define([
                 setActive($val);
                 $innerblock.scrollTop($val.position().top + $innerblock.scrollTop());
             }
-            if (config.feedback) { Cryptpad.feedback(config.feedback); }
+            if (config.feedback) { common.feedback(config.feedback); }
         };
 
         $container.click(function (e) {
