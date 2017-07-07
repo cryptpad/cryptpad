df103c4074ea878e0ddda296b7114c40e67dae65
yflory <yann.flory@xwiki.com>
2017-07-07 18:53:21 +0200

    Messaging app

--- www/friends/main.js
+++ www/friends/main.js
@@ -7,9 +7,9 @@ define([
     'css!/bower_components/components-font-awesome/css/font-awesome.min.css',
     'less!/customize/src/less/cryptpad.less',
 ], function ($, Crypto, Toolbar, Cryptpad) {
-    var Messages = Cryptpad.Messages;
+    //var Messages = Cryptpad.Messages;
 
-    var APP = {
+    var APP = window.APP = {
         Cryptpad: Cryptpad
     };
 
@@ -20,7 +20,7 @@ define([
 
         var ifrw = $('#pad-iframe')[0].contentWindow;
         var $iframe = $('#pad-iframe').contents();
-        var $appContainer = $iframe.find('#app');
+        //var $appContainer = $iframe.find('#app');
         var $list = $iframe.find('#friendList');
         var $messages = $iframe.find('#messaging');
         var $bar = $iframe.find('.toolbar-container');
@@ -37,7 +37,7 @@ define([
         var toolbar = APP.toolbar = Toolbar.create(configTb);
         toolbar.$rightside.html(''); // Remove the drawer if we don't use it to hide the toolbar
 
-        Cryptpad.getFriendListUI(Cryptpad).appendTo($list);
+        Cryptpad.initMessaging(Cryptpad, $list, $messages);
 
         Cryptpad.removeLoadingScreen();
     };
