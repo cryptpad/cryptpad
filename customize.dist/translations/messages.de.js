25cba8bba3b1ca729ac225f91bf3ebc8a31006ce
MacLemon <git@maclemon.at>
2017-07-20 20:08:30 +0200

    Translated missing out.presentSuccess to german

--- customize.dist/translations/messages.de.js
+++ customize.dist/translations/messages.de.js
@@ -53,7 +53,7 @@
       out.shareSuccess = 'URL wurde in die Zwischenablage kopiert';
 
       out.presentButtonTitle = "Präsentationsmodus starten";
-      out.presentSuccess = 'Hit ESC to exit presentation mode';
+      out.presentSuccess = 'Drücke ESC um den Präsentationsmodus zu verlassen!';
 
       out.backgroundButtonTitle = 'Die Hintergrundfarbe der Präsentation ändern';
       out.colorButtonTitle = 'Die Textfarbe im Präsentationsmodus ändern';
