0d43a84c2b94b379237ab002eb9a944dff5ea11a
ansuz <ansuz@transitiontech.ca>
2017-07-23 14:19:43 +0200

    move example into another folder

--- www/todo/example/js/template.js
+++ www/todo/example/js/template.js
@@ -58,10 +58,10 @@
 	 * });
 	 */
 	Template.prototype.show = function (data) {
-		var i, l;
+		var i = 0, l = data.length;
 		var view = '';
 
-		for (i = 0, l = data.length; i < l; i++) {
+		for (; i < l; i++) {
 			var template = this.defaultTemplate;
 			var completed = '';
 			var checked = '';
