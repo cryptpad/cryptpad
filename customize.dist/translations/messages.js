cdf6345fc14e7ce41e9171ba9d3ff2af84689bd7
Pierre Bondoerffer <pierre@bondoer.fr>
2017-08-02 17:55:05 +0200

    use sign up

--- customize.dist/translations/messages.js
+++ customize.dist/translations/messages.js
@@ -382,8 +382,6 @@ define(function () {
     out.login_invalPass = 'Password required';
     out.login_unhandledError = 'An unexpected error occurred :(';
 
-    out.login_notRegistered = 'Not registered?';
-
     out.register_importRecent = "Import pad history (Recommended)";
     out.register_acceptTerms = "I accept <a href='/terms.html' tabindex='-1'>the terms of service</a>";
     out.register_passwordsDontMatch = "Passwords do not match!";
@@ -532,12 +530,12 @@ define(function () {
 
     out.whatis_title = 'What is CryptPad';
     out.whatis_collaboration = 'Fast, Easy Collaboration';
-    out.whatis_collaboration_p1 = 'With CryptPad, you can make quick collaborative documents for taking notes and writing down ideas together. When you register and log in, you get file upload capability and a CryptDrive where you can organize all of your pads. As a registered user you get 50MB of space for free.';
+    out.whatis_collaboration_p1 = 'With CryptPad, you can make quick collaborative documents for taking notes and writing down ideas together. When you sign up and log in, you get file upload capability and a CryptDrive where you can organize all of your pads. As a registered user you get 50MB of space for free.';
     out.whatis_collaboration_p2 = 'You can share access to a CryptPad document simply by sharing the link. You can also share a link which provides <em>read only</em> access to a pad, allowing you to publicise your collaborative work while still being able to edit it.';
     out.whatis_collaboration_p3 = 'You can make simple rich text documents with <a href="http://ckeditor.com/">CKEditor</a> as well as Markdown documents which are rendered in realtime while you type. You can also use the poll app for scheduling events with multiple participants.';
     out.whatis_zeroknowledge = 'Zero Knowledge';
     out.whatis_zeroknowledge_p1 = "We don't want to know what you're typing and with modern cryptography, we make sure that we can't. CryptPad uses client side encryption to protect the content that you type from us, the people who host the server.";
-    out.whatis_zeroknowledge_p2 = 'When you register and log in, your username and password are never sent to the server, instead they are computed into a secret key using <a href="https://en.wikipedia.org/wiki/Scrypt">scrypt key derivation function</a>. This secret key is able to decrypt the content of your CryptDrive, which contains the keys to all pads that you are able to access.';
+    out.whatis_zeroknowledge_p2 = 'When you sign up and log in, your username and password are never sent to the server, instead they are computed into a secret key using <a href="https://en.wikipedia.org/wiki/Scrypt">scrypt key derivation function</a>. This secret key is able to decrypt the content of your CryptDrive, which contains the keys to all pads that you are able to access.';
     out.whatis_zeroknowledge_p3 = 'When you share the link to a document, you\'re sharing the cryptographic key for accessing that document but since the key is in the <a href="https://en.wikipedia.org/wiki/Fragment_identifier">fragment identifier</a>, it is never directly sent to the server. Check out our <a href="https://blog.cryptpad.fr/2017/07/07/cryptpad-analytics-what-we-cant-know-what-we-must-know-what-we-want-to-know/">privacy blog post</a> to learn more about what types of metadata we do and do not have access to.';
     out.whatis_drive = 'Organization with CryptDrive';
     out.whatis_drive_p1 = 'Whenever you access a pad in CryptPad, the pad is automatically added to your CryptDrive in the main folder. Later on, you can organize these pads into folders or you can put them in the trash bin. CryptDrive allows you to search through your pads and to organize them whenever you want, however you want.';
