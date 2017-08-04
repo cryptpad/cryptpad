250d85bb8ad49b2a8e51a4592dea876c48a20f8b
CatalinScr <catalin.scr@gmail.com>
2017-08-04 15:15:35 +0300

    Repaired the menu, clead the code

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -77,25 +77,19 @@ define([
     };
 
     var infopageTopbar = function () {
-        return h('div.cp-topbar',
-            h('div',
-                h('a.navbar-brand', { href: 'index.html'}, [
-                    h( 'img', { src: '/customize/CryptPad-white-logo.svg' })
-            ])
-            ),
-            h('div.navbar.navbar-toggleable-md.navbar-light.navbar-inverse',
-                h('button.navbar-toggler.navbar-toggler-right', {'type':'button'}, {'data-toggle':'collapse'}, {'data-target':'#menuCollapse'}, {'aria-controls': 'menuCollapse'}, {'aria-expanded':'false'}, {'aria-label':'Toggle navigation'},
+        return h('nav.navbar.navbar-toggleable-md',
+                     h('button.navbar-toggler.navbar-toggler-right', {'type':'button'}, {'data-toggle':'collapse'}, {'data-target':'#menuCollapse'}, {'aria-controls': 'menuCollapse'}, {'aria-expanded':'false'}, {'aria-label':'Toggle navigation'},
                     [h('i.fa.fa-bars ')
                     ]),
-                h('div.collapse.navbar-collapse.justify-content-end.flex-column#menuCollapse', [  
+                    h('a.navbar-brand', { href: '/'}),
+                h('div.collapse.navbar-collapse.justify-content-end#menuCollapse', [  
                     h('a.nav-item.nav-link', { href: '/what-is-cryptpad.html'}, Msg.topbar_whatIsCryptpad),
                     h('a.nav-item.nav-link', { href: 'https://blog.cryptpad.fr/'}, Msg.blog),
                     h('a.nav-item.nav-link', { href: '/contact.html'}, Msg.contact),
                     h('a.nav-item.nav-link', { href: '/about.html'}, Msg.about),
-                    h('a.cp-login-btn', { href: '/login'}, Msg.login_login),
-                    h('a.cp-register-btn', { href: '/register'}, Msg.login_register)
+                    h('a.nav-item.nav-link.cp-login-btn', { href: '/login'}, Msg.login_login),
+                    h('a.nav-item.nav-link.cp-register-btn', { href: '/register'}, Msg.login_register)
                 ])
-            )
         );
     };
 
@@ -280,9 +274,11 @@ define([
                                         if (showingMore) {
                                             $('.cp-more, .cp-callout-more-lessmsg').addClass('cp-hidden');
                                             $('.cp-callout-more-moremsg').removeClass('cp-hidden');
+                                            if ( $( window ).width() >991 ) $('.navbar').css('margin-top','-1em');
                                         } else {
                                             $('.cp-more, .cp-callout-more-lessmsg').removeClass('cp-hidden');
                                             $('.cp-callout-more-moremsg').addClass('cp-hidden');
+                                            $('.navbar').css('margin-top','0');
                                         }
                                         showingMore = !showingMore;
                                     }
