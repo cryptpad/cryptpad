c953e71a1891e14c97b08551e2e2828c580bde27
Pierre Bondoerffer <pierre@bondoer.fr>
2017-08-04 15:06:58 +0200

    better class generation and remove duplicate class

--- customize.dist/template.js
+++ customize.dist/template.js
@@ -80,8 +80,9 @@ $(function () {
         }
     } else {
         // add class on info-pages
-        var css = location.pathname.replace(/(\/$|^\/|.html$)/gm, "")
-            .replace(/[^a-zA-Z]/gi, "-");
+        var css = location.pathname.replace(/(index)?\.html$/gi, "") // .html
+            .replace(/[^a-zA-Z]+/gi, '-') // any non-alpha character
+            .replace(/^-|-$/g, ''); // starting/trailing dashes
         if (css === '')
         {
             css = 'index';
