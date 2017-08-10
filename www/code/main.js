9dce40f3afe925ccf4d6cca08160d106ab4e0113
Pierre Bondoerffer <pierre@bondoer.fr>
2017-08-10 14:34:48 +0200

    only add the splitter once

--- www/code/main.js
+++ www/code/main.js
@@ -388,30 +388,32 @@ define([
                 });
 
                 // add the splitter
-                var $preview = $iframe.find('#previewContainer');
-                var splitter = $('<div>', {
-                    'class': 'cp-splitter'
-                }).appendTo($preview);
+                if (!$iframe.has('.cp-splitter').length) {
+                    var $preview = $iframe.find('#previewContainer');
+                    var splitter = $('<div>', {
+                        'class': 'cp-splitter'
+                    }).appendTo($preview);
 
-                $preview.on('scroll', function() {
-                    splitter.css('top', $preview.scrollTop() + 'px');
-                });
-
-                var $target = $iframe.find('.CodeMirror');
-
-                splitter.on('mousedown', function (e) {
-                    e.preventDefault();
-                    var x = e.pageX;
-                    var w = $target.width();
-
-                    $iframe.on('mouseup mousemove', function handler(evt) {
-                        if (evt.type === 'mouseup') {
-                            $iframe.off('mouseup mousemove', handler);
-                            return;
-                        }
-                        $target.css('width', (w - x + evt.pageX) + 'px');
+                    $preview.on('scroll', function() {
+                        splitter.css('top', $preview.scrollTop() + 'px');
                     });
-                });
+
+                    var $target = $iframe.find('.CodeMirror');
+
+                    splitter.on('mousedown', function (e) {
+                        e.preventDefault();
+                        var x = e.pageX;
+                        var w = $target.width();
+
+                        $iframe.on('mouseup mousemove', function handler(evt) {
+                            if (evt.type === 'mouseup') {
+                                $iframe.off('mouseup mousemove', handler);
+                                return;
+                            }
+                            $target.css('width', (w - x + evt.pageX) + 'px');
+                        });
+                    });
+                }
 
                 Cryptpad.removeLoadingScreen();
                 setEditable(true);
