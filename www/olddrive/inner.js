e02a779c14874bba98547a38fb9367abd0963f23
yflory <yann.flory@xwiki.com>
2017-09-27 10:31:35 +0200

    Use secure drive as default drive

---
+++ www/olddrive/inner.js
@@ -0,0 +1,7 @@
+define([
+    'less!/bower_components/components-font-awesome/css/font-awesome.min.css',
+    'css!/bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'less!/drive/file.less',
+    'less!/customize/src/less/cryptpad.less',
+    'less!/customize/src/less/toolbar.less',
+], function () {});
