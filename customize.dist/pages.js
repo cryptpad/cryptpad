c5f6491c3797a52f364fe3875f1c7528f900320e
ansuz <ansuz@transitiontech.ca>
2017-11-06 15:14:02 +0100

    bump footer and package.json to latest version

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -76,7 +76,7 @@ define([
                     ])
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v1.18.0 (Snallygaster)")
+            h('div.cp-version-footer', "CryptPad v1.19.0 (Tarasque)")
         ]);
     };
 
