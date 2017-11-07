454fc8498c2c827428881f3ca8fd9eac0eea4dce
ansuz <ansuz@transitiontech.ca>
2017-11-07 13:50:07 +0100

    remove old* apps

--- www/olddrive/inner.js
+++
@@ -1,7 +0,0 @@
-define([
-    'less!/bower_components/components-font-awesome/css/font-awesome.min.css',
-    'css!/bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'less!/drive/file.less',
-    'less!/customize/src/less/cryptpad.less',
-    'less!/customize/src/less/toolbar.less',
-], function () {});
