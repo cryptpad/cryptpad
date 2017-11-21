06ede6b2942d7b11a731fc8e5e039789befc21d4
yflory <yann.flory@xwiki.com>
2017-11-21 10:26:17 +0100

    Fix export in apps using framework

--- www/common/sframe-app-framework.js
+++ www/common/sframe-app-framework.js
@@ -13,6 +13,7 @@ define([
     '/common/common-thumbnail.js',
     '/customize/application_config.js',
 
+    '/bower_components/file-saver/FileSaver.min.js',
     'css!/bower_components/bootstrap/dist/css/bootstrap.min.css',
     'less!/bower_components/components-font-awesome/css/font-awesome.min.css',
     'less!/customize/src/less2/main.less',
