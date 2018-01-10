7ced4baf074170badba93f2c1309e5cc8268fb9c
ansuz <ansuz@transitiontech.ca>
2018-01-10 09:54:10 +0100

    remove dead code from home page

--- customize.dist/main.js
+++ customize.dist/main.js
@@ -1,24 +1,11 @@
 define([
     'jquery',
-    '/customize/application_config.js',
-    '/common/cryptpad-common.js',
-    '/common/common-interface.js',
-    '/common/common-realtime.js',
-    '/common/common-constants.js',
     '/common/outer/local-store.js',
     '/customize/messages.js',
-], function ($, Config, Cryptpad, UI, Realtime, Constants, LocalStore, Messages) {
-
-    window.APP = {
-        Cryptpad: Cryptpad,
-    };
+], function ($, LocalStore, Messages) {
 
     $(function () {
         var $main = $('#mainBlock');
-
-        $(window).click(function () {
-            $('.cp-dropdown-content').hide();
-        });
 
         // main block is hidden in case javascript is disabled
         $main.removeClass('hidden');
@@ -34,113 +21,9 @@ define([
 
             $main.find('a[href="/drive/"] div.pad-button-text h4')
                 .text(Messages.main_yourCryptDrive);
-
-            var name = localStorage[Constants.userNameKey] || sessionStorage[Constants.userNameKey];
-            var $loggedInBlock = $main.find('#loggedIn');
-            var $hello = $loggedInBlock.find('#loggedInHello');
-            var $logout = $loggedInBlock.find('#loggedInLogOut');
-
-            if (name) {
-                $hello.text(Messages._getKey('login_hello', [name]));
-            } else {
-                $hello.text(Messages.login_helloNoName);
-            }
-            $('#buttons').find('.nologin').hide();
-
-            $logout.click(function () {
-                LocalStore.logout(function () {
-                    window.location.reload();
-                });
-            });
-
-            $loggedInBlock.removeClass('hidden');
         }
-        else {
-            $main.find('#userForm').removeClass('hidden');
-            $('#name').focus();
-        }
-
-        /* Log in UI */
-        var Login;
-        // deferred execution to avoid unnecessary asset loading
-        var loginReady = function (cb) {
-            if (Login) {
-                if (typeof(cb) === 'function') { cb(); }
-                return;
-            }
-            require([
-                '/common/login.js',
-            ], function (_Login) {
-                Login = Login || _Login;
-                if (typeof(cb) === 'function') { cb(); }
-            });
-        };
-
-        var $uname = $('#name').on('focus', loginReady);
-
-        var $passwd = $('#password')
-        // background loading of login assets
-        .on('focus', loginReady)
-        // enter key while on password field clicks signup
-        .on('keyup', function (e) {
-            if (e.which !== 13) { return; } // enter
-            $('button.login').click();
+        $(window).click(function () {
+            $('.cp-dropdown-content').hide();
         });
-
-        $('button.login').click(function () {
-            // setTimeout 100ms to remove the keyboard on mobile devices before the loading screen pops up
-            window.setTimeout(function () {
-                UI.addLoadingScreen({loadingText: Messages.login_hashing});
-                // We need a setTimeout(cb, 0) otherwise the loading screen is only displayed after hashing the password
-                window.setTimeout(function () {
-                    loginReady(function () {
-                        var uname = $uname.val();
-                        var passwd = $passwd.val();
-                        Login.loginOrRegister(uname, passwd, false, function (err, result) {
-                            if (!err) {
-                                var proxy = result.proxy;
-
-                                // successful validation and user already exists
-                                // set user hash in localStorage and redirect to drive
-                                if (proxy && !proxy.login_name) {
-                                    proxy.login_name = result.userName;
-                                }
-
-                                proxy.edPrivate = result.edPrivate;
-                                proxy.edPublic = result.edPublic;
-
-                                Realtime.whenRealtimeSyncs(result.realtime, function () {
-                                    LocalStore.login(result.userHash, result.userName, function () {
-                                        document.location.href = '/drive/';
-                                    });
-                                });
-                                return;
-                            }
-                            switch (err) {
-                                case 'NO_SUCH_USER':
-                                    UI.removeLoadingScreen(function () {
-                                        UI.alert(Messages.login_noSuchUser);
-                                    });
-                                    break;
-                                case 'INVAL_USER':
-                                    UI.removeLoadingScreen(function () {
-                                        UI.alert(Messages.login_invalUser);
-                                    });
-                                    break;
-                                case 'INVAL_PASS':
-                                    UI.removeLoadingScreen(function () {
-                                        UI.alert(Messages.login_invalPass);
-                                    });
-                                    break;
-                                default: // UNHANDLED ERROR
-                                    UI.errorLoadingScreen(Messages.login_unhandledError);
-                            }
-                        });
-                    });
-                }, 0);
-            }, 100);
-        });
-        /* End Log in UI */
-        console.log("ready");
     });
 });
