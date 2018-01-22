c90ba890ca2599d6667396293d58569e35a47902
yflory <yann.flory@xwiki.com>
2018-01-22 16:56:37 +0100

    Add more configurable options and add a new way to use custom config

--- customize.dist/application_config.js
+++ customize.dist/application_config.js
@@ -1,74 +1,10 @@
-define(function() {
-    var config = {};
-
-    /* Select the buttons displayed on the main page to create new collaborative sessions
-     * Existing types : pad, code, poll, slide
-     */
-    config.availablePadTypes = ['drive', 'pad', 'code', 'slide', 'poll', 'whiteboard', 'file', 'todo', 'contacts'];
-    config.registeredOnlyTypes = ['file', 'contacts'];
-
-    /*  Cryptpad apps use a common API to display notifications to users
-     *  by default, notifications are hidden after 5 seconds
-     *  You can change their duration here (measured in milliseconds)
-     */
-    config.notificationTimeout = 5000;
-    config.disableUserlistNotifications = false;
-    config.hideLoadingScreenTips = false;
-
-    config.enablePinning = true;
-
-    config.whiteboardPalette = [
-        '#000000', // black
-        '#FFFFFF', // white
-        '#848484', // grey
-        '#8B4513', // saddlebrown
-        '#FF0000', // red
-        '#FF8080', // peach?
-        '#FF8000', // orange
-        '#FFFF00', // yellow
-        '#80FF80', // light green
-        '#00FF00', // green
-        '#00FFFF', // cyan
-        '#008B8B', // dark cyan
-        '#0000FF', // blue
-        '#FF00FF', // fuschia
-        '#FF00C0', // hot pink
-        '#800080', // purple
-    ];
-
-    config.enableTemplates = true;
-
-    config.enableHistory = true;
-
-    /*  user passwords are hashed with scrypt, and salted with their username.
-        this value will be appended to the username, causing the resulting hash
-        to differ from other CryptPad instances if customized. This makes it
-        such that anyone who wants to bruteforce common credentials must do so
-        again on each CryptPad instance that they wish to attack.
-
-        WARNING: this should only be set when your CryptPad instance is first
-        created. Changing it at a later time will break logins for all existing
-        users.
-    */
-    config.loginSalt = '';
-    config.minimumPasswordLength = 8;
-
-    config.badStateTimeout = 30000;
-
-    config.applicationsIcon = {
-        file: 'fa-file-text-o',
-        pad: 'fa-file-word-o',
-        code: 'fa-file-code-o',
-        slide: 'fa-file-powerpoint-o',
-        poll: 'fa-calendar',
-        whiteboard: 'fa-paint-brush',
-        todo: 'fa-tasks',
-        contacts: 'fa-users',
-    };
-
-    config.displayCreationScreen = false;
-
-    config.disableAnonymousStore = false;
-
-    return config;
+/*
+ * You can override the configurable values from this file.
+ * The recommended method is to make a copy of this file (/customize.dist/application_config.js)
+   in a 'customize' directory (/customize/application_config.js).
+ * If you want to check all the configurable values, you can open the internal configuration file
+   but you should not change it directly (/common/application_config_internal.js)
+*/
+define(['/common/application_config_internal.js'], function (AppConfig) {
+    return AppConfig;
 });
---
+++ www/common/application_config_internal.js
@@ -0,0 +1,79 @@
+/*
+ * This is an internal configuration file.
+ * If you want to change some configurable values, use the '/customize/application_config.js'
+ * file (make a copy from /customize.dist/application_config.js)
+ */
+define(function() {
+    var config = {};
+
+    /* Select the buttons displayed on the main page to create new collaborative sessions
+     * Existing types : pad, code, poll, slide
+     */
+    config.availablePadTypes = ['drive', 'pad', 'code', 'slide', 'poll', 'whiteboard', 'file', 'todo', 'contacts'];
+    config.registeredOnlyTypes = ['file', 'contacts'];
+
+    /*  Cryptpad apps use a common API to display notifications to users
+     *  by default, notifications are hidden after 5 seconds
+     *  You can change their duration here (measured in milliseconds)
+     */
+    config.notificationTimeout = 5000;
+    config.disableUserlistNotifications = false;
+    config.hideLoadingScreenTips = false;
+
+    config.enablePinning = true;
+
+    config.whiteboardPalette = [
+        '#000000', // black
+        '#FFFFFF', // white
+        '#848484', // grey
+        '#8B4513', // saddlebrown
+        '#FF0000', // red
+        '#FF8080', // peach?
+        '#FF8000', // orange
+        '#FFFF00', // yellow
+        '#80FF80', // light green
+        '#00FF00', // green
+        '#00FFFF', // cyan
+        '#008B8B', // dark cyan
+        '#0000FF', // blue
+        '#FF00FF', // fuschia
+        '#FF00C0', // hot pink
+        '#800080', // purple
+    ];
+
+    config.enableTemplates = true;
+
+    config.enableHistory = true;
+
+    /*  user passwords are hashed with scrypt, and salted with their username.
+        this value will be appended to the username, causing the resulting hash
+        to differ from other CryptPad instances if customized. This makes it
+        such that anyone who wants to bruteforce common credentials must do so
+        again on each CryptPad instance that they wish to attack.
+
+        WARNING: this should only be set when your CryptPad instance is first
+        created. Changing it at a later time will break logins for all existing
+        users.
+    */
+    config.loginSalt = '';
+    config.minimumPasswordLength = 8;
+
+    config.badStateTimeout = 30000;
+
+    config.applicationsIcon = {
+        file: 'fa-file-text-o',
+        pad: 'fa-file-word-o',
+        code: 'fa-file-code-o',
+        slide: 'fa-file-powerpoint-o',
+        poll: 'fa-calendar',
+        whiteboard: 'fa-paint-brush',
+        todo: 'fa-tasks',
+        contacts: 'fa-users',
+    };
+
+    config.displayCreationScreen = false;
+
+    config.disableAnonymousStore = false;
+
+    return config;
+});
