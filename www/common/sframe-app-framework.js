3d95e6088a012eb9922aeadb1a6e900e156dd324
ansuz <ansuz@transitiontech.ca>
2018-07-16 11:24:58 +0200

    leave a FIXME note

--- www/common/sframe-app-framework.js
+++ www/common/sframe-app-framework.js
@@ -126,7 +126,7 @@ define([
             if (newState === STATE.INFINITE_SPINNER || newState === STATE.DELETED) {
                 state = newState;
             } else if (state === STATE.DISCONNECTED && newState !== STATE.INITIALIZING) {
-                throw new Error("Cannot transition from DISCONNECTED to " + newState);
+                throw new Error("Cannot transition from DISCONNECTED to " + newState); // FIXME we are getting "DISCONNECTED to READY" on prod
             } else if (state !== STATE.READY && newState === STATE.HISTORY_MODE) {
                 throw new Error("Cannot transition from " + state + " to " + newState);
             } else {
