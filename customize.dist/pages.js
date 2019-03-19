285ee17ed2007fde0649e7f6f817713683ef265c
ansuz <ansuz@transitiontech.ca>
2019-03-19 14:08:17 +0100

    increment version to 2.19.0 (Tapir)

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -91,7 +91,7 @@ define([
                     ])
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v2.18.0 (Sloth)")
+            h('div.cp-version-footer', "CryptPad v2.19.0 (Tapir)")
         ]);
     };
 
