11236c6445eebe3fa2364b7f86c33fafd7583bc9
yflory <yann.flory@xwiki.com>
2019-03-29 11:33:49 +0100

    Fix docker issues

--- container-start.sh
+++ container-start.sh
@@ -4,8 +4,12 @@
 mkdir -p customize
 
 # Copying default config
-mkdir -p config
-[ ! -f config/config.js ] && echo "Creating config.js" && cp config/config.example.js config/config.js
+mkdir -p cfg
+[ ! -f cfg/config.js ] && echo "Creating config.js" && cp config/config.example.js cfg/config.js
+
+# Linking config.js
+[ ! -L config/config.js ] && echo "Linking config.js" && ln -s ../cfg/config.js config/config.js
+
 
 # Thanks to http://stackoverflow.com/a/10467453
 sedeasy() {
@@ -14,13 +18,13 @@ sedeasy() {
 
 # Configure
 [ -n "$USE_SSL" ] && echo "Using secure websockets: $USE_SSL" \
-  && sedeasy "useSecureWebsockets: [^,]*," "useSecureWebsockets: ${USE_SSL}," config/config.js
+  && sedeasy "useSecureWebsockets: [^,]*," "useSecureWebsockets: ${USE_SSL}," cfg/config.js
 
 [ -n "$STORAGE" ] && echo "Using storage adapter: $STORAGE" \
-  && sedeasy "storage: [^,]*," "storage: ${STORAGE}," config/config.js
+  && sedeasy "storage: [^,]*," "storage: ${STORAGE}," cfg/config.js
 
 [ -n "$LOG_TO_STDOUT" ] && echo "Logging to stdout: $LOG_TO_STDOUT" \
-  && sedeasy "logToStdout: [^,]*," "logToStdout: ${LOG_TO_STDOUT}," config/config.js
+  && sedeasy "logToStdout: [^,]*," "logToStdout: ${LOG_TO_STDOUT}," cfg/config.js
 
 export FRESH=1
 exec node ./server.js
