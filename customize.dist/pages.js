a67e5e0f696ac7148df38257a2175ec1a4a73921
yflory <yann.flory@xwiki.com>
2019-05-20 17:44:18 +0200

    update package.json and footer

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -103,7 +103,7 @@ define([
                     ])*/
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v2.21.0 (Vervet)")
+            h('div.cp-version-footer', "CryptPad v2.22.0 (Wolf)")
         ]);
     };
 
