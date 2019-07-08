8be6c84e2f5aebbf58384b5b6b440e11c12e1d64
ansuz <ansuz@transitiontech.ca>
2019-07-08 12:10:57 +0200

    guard against null elements in the drive

--- www/common/outer/userObject.js
+++ www/common/outer/userObject.js
@@ -506,8 +506,14 @@ define([
             var fixRoot = function (elem) {
                 if (typeof(files[ROOT]) !== "object") { debug("ROOT was not an object"); files[ROOT] = {}; }
                 var element = elem || files[ROOT];
+                if (!element) { return console.error("Invalid element in root");
                 var nbMetadataFolders = 0;
                 for (var el in element) {
+                    if (element[el] === null) {
+                        console.error('element[%s] is null', el);
+                        delete element[el];
+                        continue;
+                    }
                     if (exp.isFolderData(element[el])) {
                         if (nbMetadataFolders !== 0) {
                             debug("Multiple metadata files in folder");
