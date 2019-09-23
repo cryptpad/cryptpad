33623bdafc207e2e186fad84a9a0d20e8a4f8600
ansuz <ansuz@transitiontech.ca>
2019-09-23 11:13:17 +0200

    update version strings

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -103,7 +103,7 @@ define([
                     ])*/
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v3.1.0 (Baiji)")
+            h('div.cp-version-footer', "CryptPad v3.2.0 (Chilihueque)")
         ]);
     };
 
