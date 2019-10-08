78bd5441964c2a2574fb9ade2d72810914b469c3
yflory <yann.flory@xwiki.com>
2019-10-08 13:54:48 +0200

    bump version

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -103,7 +103,7 @@ define([
                     ])*/
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v3.2.0 (Chilihueque)")
+            h('div.cp-version-footer', "CryptPad v3.3.0 (Dodo)")
         ]);
     };
 
