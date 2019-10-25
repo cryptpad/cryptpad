c59d41d8f6ba6fb81c1917442b7be2a24ad067b2
yflory <yann.flory@xwiki.com>
2019-10-25 18:25:32 +0200

    Fix Ctrl+E menu in some parts of the drive

--- www/common/common-ui-elements.js
+++ www/common/common-ui-elements.js
@@ -2790,7 +2790,9 @@ define([
 
     UIElements.createNewPadModal = function (common) {
         // if in drive, show new pad modal instead
-        if ($("body.cp-app-drive").length !== 0) { return void $(".cp-app-drive-element-row.cp-app-drive-new-ghost").click(); }
+        if ($(".cp-app-drive-element-row.cp-app-drive-new-ghost").length !== 0) {
+            return void $(".cp-app-drive-element-row.cp-app-drive-new-ghost").click();
+        }
 
         var $modal = UIElements.createModal({
             id: 'cp-app-toolbar-creation-dialog',
