d52acfa5d270fadc430e7ef5ec025a43e229196c
yflory <yann.flory@xwiki.com>
2019-11-29 17:02:02 +0100

    Change initial tab in the file share modal

--- www/common/common-ui-elements.js
+++ www/common/common-ui-elements.js
@@ -1349,13 +1349,15 @@ define([
 
         // Create modal
         var tabs = [{
-            title: Messages.share_linkCategory,
-            icon: "fa fa-link",
-            content: frameLink
-        }, {
             title: Messages.share_contactCategory,
             icon: "fa fa-address-book",
-            content: frameContacts
+            content: frameContacts,
+            active: hasFriends,
+        }, {
+            title: Messages.share_linkCategory,
+            icon: "fa fa-link",
+            content: frameLink,
+            active: !hasFriends
         }, {
             title: Messages.share_embedCategory,
             icon: "fa fa-code",
