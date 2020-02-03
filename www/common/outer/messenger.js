3601bd6429e1eeaf610ef2e31daa5bad8defb4a3
ansuz <ansuz@transitiontech.ca>
2020-02-03 16:46:09 -0500

    leave an XXX note to make sure we fix this typeError

--- www/common/outer/messenger.js
+++ www/common/outer/messenger.js
@@ -893,6 +893,8 @@ define([
     };
 
     var clearOwnedChannel = function (ctx, id, cb) {
+        // XXX clients is undefined if you try to clear history from contacts
+        // while not using workers
         var channel = ctx.clients[id];
         if (!channel) { return void cb({error: 'NO_CHANNEL'}); }
         if (!ctx.store.rpc) { return void cb({error: 'RPC_NOT_READY'}); }
