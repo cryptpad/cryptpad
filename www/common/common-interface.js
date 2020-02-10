532333d42734d6f6cb4b52ac48efc1a65cdd563f
yflory <yann.flory@xwiki.com>
2020-02-10 17:58:39 +0100

    Fix keyboard shortcut in confirm

--- www/common/common-interface.js
+++ www/common/common-interface.js
@@ -587,7 +587,7 @@ define([
             $ok.click();
         }, function () {
             $cancel.click();
-        }, ok);
+        });
 
         document.body.appendChild(frame);
         setTimeout(function () {
