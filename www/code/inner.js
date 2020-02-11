d58244b4c4fc46044d760e2f4ed5ef81c1329bec
yflory <yann.flory@xwiki.com>
2020-02-11 18:19:16 +0100

    Support relative url and anchor url in code

--- www/code/inner.js
+++ www/code/inner.js
@@ -170,6 +170,14 @@ define([
                 e.preventDefault();
                 var $a = $t.is('a') ? $t : $t.parents('a').first();
                 var href = $a.attr('href');
+                if (/^\/[^\/]/.test(href)) {
+                    var privateData = common.getMetadataMgr().getPrivateData();
+                    href = privateData.origin + href;
+                } else if (/^#/.test(href)) {
+                    var target = document.getElementById(href.slice(1));
+                    target.scrollIntoView();
+                    return;
+                }
                 framework._.sfCommon.openUnsafeURL(href);
             }
         });
