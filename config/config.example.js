59ad80d7f16f1627512dd1a826badc27569b1751
ansuz <ansuz@transitiontech.ca>
2020-02-26 13:09:11 -0500

    support larger upload sizes for users with premium accounts

--- config/config.example.js
+++ config/config.example.js
@@ -1,4 +1,3 @@
-/*@flow*/
 /*
     globals module
 */
@@ -250,6 +249,9 @@ module.exports = {
      *  anything larger than this size will be rejected
      */
     maxUploadSize: 20 * 1024 * 1024,
+
+    // XXX
+    premiumUploadSize: 100 * 1024 * 1024,
 
     /* =====================
      *    HARDWARE RELATED
