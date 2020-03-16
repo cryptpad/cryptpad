cdcc52224aead1b9e0b322510d9cfc6aab22b83d
yflory <yann.flory@xwiki.com>
2020-03-16 16:46:09 +0100

    Reload when the API version changes

--- www/common/cryptpad-common.js
+++ www/common/cryptpad-common.js
@@ -1712,9 +1712,9 @@ define([
         var stored = currentVersion || '0.0.0';
         var storedArr = stored.split('.');
         storedArr[2] = 0;
-        var shouldUpdate = parseInt(verArr[0]) > parseInt(storedArr[0]) ||
+        var shouldUpdate = parseInt(verArr[0]) !== parseInt(storedArr[0]) ||
                            (parseInt(verArr[0]) === parseInt(storedArr[0]) &&
-                            parseInt(verArr[1]) > parseInt(storedArr[1]));
+                            parseInt(verArr[1]) !== parseInt(storedArr[1]));
         if (!shouldUpdate) { return; }
         currentVersion = ver;
         localStorage[CRYPTPAD_VERSION] = ver;
