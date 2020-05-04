05f164a2e51333f95cc6d885561118756347839b
yflory <yann.flory@xwiki.com>
2020-05-04 12:20:18 +0200

    Fix issue with the addComment button in the toolbar

--- www/pad/comments.js
+++ www/pad/comments.js
@@ -896,7 +896,8 @@ define([
             if ($target.closest('.ui-autocomplete').length) { return; }
             // Add comment button? don't remove anything because this handler is called after
             // the button action
-            if (e.target.classList.contains('cke_button__comment_icon')) { return; }
+            if ($target.is('.cke_button__comment')) { return; }
+            if ($target.closest('.cke_button__comment').length) { return; }
             Env.$container.find('.cp-comment-active').removeClass('cp-comment-active');
             Env.$inner.find('comment.active').removeClass('active');
             Env.$container.find('.cp-comment-form').remove();
