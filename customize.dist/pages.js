b9d4f252ebcbdef360373bc07e3c67613d9c4556
ansuz <ansuz@transitiontech.ca>
2020-10-09 14:58:08 +0530

    update version string

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -62,7 +62,7 @@ define([
     var imprintUrl = AppConfig.imprint && (typeof(AppConfig.imprint) === "boolean" ?
                         '/imprint.html' : AppConfig.imprint);
 
-    Pages.versionString = "CryptPad v3.22.0 (WoollyMammoth)";
+    Pages.versionString = "CryptPad v3.23.0 (XerusDaamsi)";
 
     // used for the about menu
     Pages.imprintLink = AppConfig.imprint ? footLink(imprintUrl, 'imprint') : undefined;
