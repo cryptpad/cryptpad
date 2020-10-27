c06c8c01f1f43832965675fd85fe6566a05628de
David Benqué <mail@davidbenque.com>
2020-10-27 12:08:55 +0000

    Clean up XXXs

--- customize.dist/pages/index.js
+++ customize.dist/pages/index.js
@@ -110,14 +110,15 @@ define([
             Feedback.send('HOME_SUPPORT_CRYPTPAD');
         });
 
-        Msg.home_privacy_title = "Private by design"; // XXX
-        Msg.home_privacy_text = "CryptPad is built to enable collaboration while keeping data private. All information including documents, chats, and files is encrypted and decrypted by your browser. This means nothing is readable outside of the session where you are logged in. Even the service administrators do not have access to your information."; // XXX
-        Msg.home_host_title = "About this instance"; // XXX
-        Msg.home_host = "This is an independent community instance of CryptPad." // XXX existing key
-        Msg.home_opensource_title = "Open Source"; // XXX
-        Msg.home_opensource = 'Anyone can host CryptPad and offer the service in a personal or professional capacity. The source code is available on <a href="https://github.com/xwiki-labs/cryptpad">Github</a>.'; // XXX
-        Msg.home_support_title = "Support CryptPad"; // XXX
-        Msg.home_support = "<p>CryptPad does not profit from user's data. This is part of a vision for online services that respect privacy. Instead of pretending to be \"free\" like the big platforms, CryptPad aims to build a sustainable model: funded willingly by users instead of making profits from personal information.</p><p>You can support the project by making a one-time or recurring donation through our Open Collective. Our budget is transparent and updates are published regularly. There are also a number of <a href=\"https://docs.cryptpad.fr/en/how_to_contribute.html\" rel=\"noopener noreferrer\" target=\"_blank\">non-financial ways to contribute</a>.</p>" // XXX
+        // XXX
+        Msg.home_privacy_title = "Private by design";
+        Msg.home_privacy_text = "CryptPad is built to enable collaboration while keeping data private. All information including documents, chats, and files is encrypted and decrypted by your browser. This means nothing is readable outside of the session where you are logged in. Even the service administrators do not have access to your information.";
+        Msg.home_host_title = "About this instance";
+        Msg.home_host = "This is an independent community instance of CryptPad." // existing key
+        Msg.home_opensource_title = "Open Source";
+        Msg.home_opensource = 'Anyone can host CryptPad and offer the service in a personal or professional capacity. The source code is available on <a href="https://github.com/xwiki-labs/cryptpad">Github</a>.';
+        Msg.home_support_title = "Support CryptPad";
+        Msg.home_support = "<p>CryptPad does not profit from user's data. This is part of a vision for online services that respect privacy. Instead of pretending to be \"free\" like the big platforms, CryptPad aims to build a sustainable model: funded willingly by users instead of making profits from personal information.</p><p>You can support the project by making a one-time or recurring donation through our Open Collective. Our budget is transparent and updates are published regularly. There are also a number of <a href=\"https://docs.cryptpad.fr/en/how_to_contribute.html\" rel=\"noopener noreferrer\" target=\"_blank\">non-financial ways to contribute</a>.</p>"
 
         var blocks = [
             h('div.row.cp-index-section', [
--- customize.dist/pages/what-is-cryptpad.js
+++ customize.dist/pages/what-is-cryptpad.js
@@ -6,22 +6,20 @@ define([
 ], function (Config, h, Msg, Pages) {
     var urlArgs = Config.requireConf.urlArgs;
 
-    Msg.whatis_collaboration = "Private Collaboration"; // XXX existing key
-    Msg.whatis_collaboration_info = '<p>CryptPad is built to enable collaboration, synchronizing in real time between users editing the same document, but has no access to the content of the document or data about users. Because all data is encrypted, the service and its administrators have no way of seeing the content being edited and stored.</p><p>Collaborating in real time on online documents is now a common thing. A range of well known internet platforms offer this service. In order to enable collaboration, these services synchronize changes between all users. In the process they gain access to the content of the document and to data about the behaviour of users. While these services are often advertised as "free", platforms monetise user data by using it to profile users and selling advertising.</p>'; // XXX
+    // XXX
+    Msg.whatis_collaboration = "Private Collaboration"; // existing key
+    Msg.whatis_collaboration_info = '<p>CryptPad is built to enable collaboration, synchronizing in real time between users editing the same document, but has no access to the content of the document or data about users. Because all data is encrypted, the service and its administrators have no way of seeing the content being edited and stored.</p><p>Collaborating in real time on online documents is now a common thing. A range of well known internet platforms offer this service. In order to enable collaboration, these services synchronize changes between all users. In the process they gain access to the content of the document and to data about the behaviour of users. While these services are often advertised as "free", platforms monetise user data by using it to profile users and selling advertising.</p>';
     // XXX remove whatis_collaboration_p1, p2, p3
-
-    Msg.whatis_apps = "A full suite of applications"; // XXX
-    Msg.whatis_apps_info = "<p>CryptPad provides a full-fledged office suite, with all the tools necessary for productive collaboration. Applications include: Rich Text, Spreadsheets, Code/Markdown, Kanban, Slides, Whiteboard and Polls.</p><p> A secure chat is available in each document for secure communication, [continue ...]</p>"; // XXX
+    Msg.whatis_apps = "A full suite of applications";
+    Msg.whatis_apps_info = "<p>CryptPad provides a full-fledged office suite, with all the tools necessary for productive collaboration. Applications include: Rich Text, Spreadsheets, Code/Markdown, Kanban, Slides, Whiteboard and Polls.</p><p> A secure chat is available in each document for secure communication, [continue ...]</p>";
     // XXX remove all whatis_zeroknowledge keys
-
-    Msg.whatis_drive_info = "<p>Manage documents with CryptDrive. Create folders, shared folders, tags, [continue ...]</p>" // XXX
+    Msg.whatis_drive_info = "<p>Manage documents with CryptDrive. Create folders, shared folders, tags, [continue ...]</p>"
     // XXX remove whatis_drive_p1, p2, p3
-
-    Msg.whatis_model = "Business model"; // XXX
-    Msg.whatis_model_info = "<p>CryptPad is open source...</p><p>CryptPad does not profit from its users data. This is because being fully encrypted it does not gather any useful data that could be sold to profile users. This lack of data is a feature, not a bug, it is part of a vision for online services that respect users privacy. Instead of pretending to be \"free\" like the big platforms CryptPad aims to build a financially sustainable model: funded willingly by users instead of profiting form personal information.</p><p>Since 2016, CryptPad is supported by French and European research grants such as BPI France, NLNet Foundation, NGI Trust, Mozilla Open Source Support, as well as donations and subscriptions to the service. Now that the feasibility of the project has been established, the next goal is to make financially sustainable through user funding. If you would like to support CryptPad and help make it a sustainable alternative to the big platforms, please consider making a donation.</p>" // XXX
-
-    Msg.whatis_xwiki = "Made at XWiki"; // XXX
+    Msg.whatis_model = "Business model";
+    Msg.whatis_model_info = "<p>CryptPad is open source [continue ...]</p><p>CryptPad does not profit from its users data. This is because being fully encrypted it does not gather any useful data that could be sold to profile users. This lack of data is a feature, not a bug, it is part of a vision for online services that respect users privacy. Instead of pretending to be \"free\" like the big platforms CryptPad aims to build a financially sustainable model: funded willingly by users instead of profiting form personal information.</p><p>Since 2016, CryptPad is supported by French and European research grants such as BPI France, NLNet Foundation, NGI Trust, Mozilla Open Source Support, as well as donations and subscriptions to the service. Now that the feasibility of the project has been established, the next goal is to make financially sustainable through user funding. If you would like to support CryptPad and help make it a sustainable alternative to the big platforms, please consider making a donation.</p>"
+    Msg.whatis_xwiki = "Made at XWiki";
     Msg.whatis_xwiki_info = "<p>CryptPad is made at XWiki, a company based in Paris that has been making open-source software for over 15 years. [continue ...]</p>"
+    // XXX remove all whatis_business keys
     return function () {
         return h('div#cp-main', [
             Pages.infopageTopbar(),
@@ -86,7 +84,6 @@ define([
                         }),
                     ]),
                 ]),
-                // XXX XWiki info
                 h('div.row.cp-page-section', [
                     h('div.col-md-6', [
                         Pages.setHTML(h('h2'), Msg.whatis_xwiki),
