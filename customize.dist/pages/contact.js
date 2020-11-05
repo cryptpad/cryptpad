d2940f61da4f7fe5b43533313dddb00a726a94b5
David Benqué <mail@davidbenque.com>
2020-11-05 11:03:23 +0000

    Contact page

--- customize.dist/pages/contact.js
+++ customize.dist/pages/contact.js
@@ -2,54 +2,85 @@ define([
     '/api/config',
     '/common/hyperscript.js',
     '/customize/messages.js',
-    '/customize/pages.js'
-], function (Config, h, Msg, Pages) {
+    '/customize/pages.js',
+    '/common/outer/local-store.js'
+], function (Config, h, Msg, Pages, LocalStore) {
+
+    // XXX
+    Msg.contact_adminHint = "For any issues related to your account, storage limit, or availability of the service." // existing key
+
+
     return function () {
+        var adminEmail = Config.adminEmail && Config.adminEmail !== 'i.did.not.read.my.config@cryptpad.fr';
+        var adminMailbox = Config.supportMailbox;
         return h('div#cp-main', [
             Pages.infopageTopbar(),
-            h('div.container-fluid.cp-contdet', [
-                h('row.col-12.col-sm-12',
-                    h('h1.text-center', Msg.contact )
-                )
-            ]),
             h('div.container.cp-container', [
-                Config.adminEmail && Config.adminEmail !== 'i.did.not.read.my.config@cryptpad.fr' ? h('div.row.cp-iconCont.align-items-center', [
+                h('div.row.cp-page-title',[
+                    h('div.col-12.text-center', h('h1', Msg.contact)),
+                ]),
+                (adminEmail || adminMailbox) ? h('div.row.cp-iconCont.align-items-center', [
                     h('div.col-12',
-                        Pages.setHTML(h('h4.text-center'), Msg.contact_admin),
+                        Pages.setHTML(h('h2.text-center'), Msg.contact_admin),
                         h('p', Msg.contact_adminHint)
                     ),
-                    h('div.col-12.col-sm-6.col-md-3.col-lg-3',
+                    adminEmail ? h('div.col-12.col-sm-6.col-md-3.col-lg-3',
                         h('a.card', {href : "mailto:"+Config.adminEmail},
                             h('div.card-body',
                                 h('p', [
-                                    h('img', {src: '/customize/images/email.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/email.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'
+                                    }),
                                     Msg.contact_email || 'Email'
                                 ])
                             )
                         )
-                    ),
+                    ) : undefined,
+                    (adminMailbox && LocalStore.isLoggedIn()) ? h('div.col-12.col-sm-6.col-md-3.col-lg-3', // XXX show "disabled" support if not logged in
+                        h('a.card', {href : "/support/"},
+                            h('div.card-body',
+                                h('p', [
+                                    h('img', {
+                                        src: '/customize/images/support.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'
+                                    }),
+                                    Msg.supportPage || 'Support'
+                                ])
+                            )
+                        )
+                    ) : undefined,
                 ]) : undefined,
                 h('div.row.cp-iconCont.align-items-center', [
                     h('div.col-12',
-                        Pages.setHTML(h('h4.text-center'), Msg.contact_dev),
+                        Pages.setHTML(h('h2.text-center'), Msg.contact_dev),
                         h('p', Msg.contact_devHint)
                     ),
                     h('div.col-12.col-sm-6.col-md-3.col-lg-3',
-                        h('a.card', {href : "https://twitter.com/cryptpad"}, 
+                        h('a.card', {href : "https://twitter.com/cryptpad"},
                             h('div.card-body',
                                 h('p', [
                                     // this is not a typo. adblock plus blocks images with src *twitter* apparently
-                                    h('img', {src: '/customize/images/twiitter.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/twiitter.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'}),
                                     'Twitter'
                                 ])
                             )
                         )
                     ),
                     h('div.col-12.col-sm-6.col-md-3.col-lg-3',
-                        h('a.card', {href : "https://social.weho.st/@cryptpad"}, 
+                        h('a.card', {href : "https://social.weho.st/@cryptpad"},
                             h('div.card-body',
                                 h('p', [
-                                    h('img', {src: '/customize/images/mastodon.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/mastodon.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'
+                                    }),
                                     'Mastodon'
                                 ])
                             )
@@ -59,17 +90,24 @@ define([
                         h('a.card', {href : "https://github.com/xwiki-labs/cryptpad/issues/"},
                             h('div.card-body',
                                 h('p', [
-                                    h('img', {src: '/customize/images/issue.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/github.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'}),
                                     Msg.contact_bug || 'Bug report'
                                 ])
                             )
                         )
                     ),
                     h('div.col-12.col-sm-6.col-md-3.col-lg-3',
-                        h('a.card', {href : "https://riot.im/app/#/room/#cryptpad:matrix.org"},
+                        h('a.card', {href : "https://app.element.io/#/room/#cryptpad:matrix.xwiki.com"},
                             h('div.card-body',
                                 h('p', [
-                                    h('img', {src: '/customize/images/sayhi.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/sayhi.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'
+                                    }),
                                     Msg.contact_chat || 'Chat'
                                 ])
                             )
@@ -79,7 +117,11 @@ define([
                         h('a.card', {href : "mailto:contact@cryptpad.fr"},
                             h('div.card-body',
                                 h('p', [
-                                    h('img', {src: '/customize/images/email.svg'}),
+                                    h('img', {
+                                        src: '/customize/images/email.svg',
+                                        alt: '',
+                                        'aria-hidden': 'true'
+                                    }),
                                     Msg.contact_email || 'Email'
                                 ])
                             )
