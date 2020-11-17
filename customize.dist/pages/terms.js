3599b33815cb22f4cceba70bf1fb22a834c81698
David Benqué <mail@davidbenque.com>
2020-11-17 13:14:28 +0000

    Fix terms and privacy pages

--- customize.dist/pages/terms.js
+++ customize.dist/pages/terms.js
@@ -7,12 +7,14 @@ define([
         return h('div#cp-main', [
             Pages.infopageTopbar(),
             h('div.container.cp-container', [
-                h('center', h('h1', Msg.tos_title)),
-                h('p', Msg.tos_legal),
-                h('p', Msg.tos_availability),
-                h('p', Msg.tos_e2ee),
-                h('p', Msg.tos_logs),
-                h('p', Msg.tos_3rdparties),
+                h('.row.cp-page-title', h('h1', Msg.tos_title)),
+                h('.row', [
+                    h('p', Msg.tos_legal),
+                    h('p', Msg.tos_availability),
+                    h('p', Msg.tos_e2ee),
+                    h('p', Msg.tos_logs),
+                    h('p', Msg.tos_3rdparties),
+                ])
             ]),
             Pages.infopageFooter()
         ]);
