16e4b76f4ba524fba705a210f105817a31d02acc
David Benqué <mail@davidbenque.com>
2021-02-04 17:44:29 +0000

    Dark toolbar and apps

--- www/slide/inner.js
+++ www/slide/inner.js
@@ -310,7 +310,7 @@ define([
 
             var $nav = $('<nav>').appendTo($div);
             $('<button>', {'class': 'btn cancel'}).text(Messages.cancelButton).appendTo($nav).click(todoCancel);
-            $('<button>', {'class': 'btn ok'}).text(Messages.settings_save).appendTo($nav).click(todo);
+            $('<button>', {'class': 'btn primary'}).text(Messages.settings_save).appendTo($nav).click(todo);
 
             return $container;
         };
