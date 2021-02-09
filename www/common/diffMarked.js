d72e7cf8c254bdb4f40bd3aab2e1ead92f783b1c
yflory <yann.flory@xwiki.com>
2021-02-09 13:32:09 +0100

    Fix codemirror dracula

--- www/common/diffMarked.js
+++ www/common/diffMarked.js
@@ -11,7 +11,7 @@ define([
     '/common/highlight/highlight.pack.js',
     '/bower_components/diff-dom/diffDOM.js',
     '/bower_components/tweetnacl/nacl-fast.min.js',
-    'css!/common/highlight/styles/'+ (window.CryptPad_theme === 'dark' ? 'dracula.css' : 'github.css')
+    'css!/common/highlight/styles/'+ (window.CryptPad_theme === 'dark' ? 'dark.css' : 'github.css')
 ],function ($, ApiConfig, Marked, Hash, Util, h, MT, MediaTag, Messages) {
     var DiffMd = {};
 
