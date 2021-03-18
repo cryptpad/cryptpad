2279898d1084592775543c0c9dccf40d814dfbd9
ansuz <ansuz@transitiontech.ca>
2021-03-18 10:26:21 +0530

    increment version string for 4.3.0

--- customize.dist/pages.js
+++ customize.dist/pages.js
@@ -62,7 +62,7 @@ define([
     var imprintUrl = AppConfig.imprint && (typeof(AppConfig.imprint) === "boolean" ?
                         '/imprint.html' : AppConfig.imprint);
 
-    Pages.versionString = "v4.2.1";
+    Pages.versionString = "v4.3.0";
 
     // used for the about menu
     Pages.imprintLink = AppConfig.imprint ? footLink(imprintUrl, 'imprint') : undefined;
