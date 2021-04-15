3a5b3348991409ebafcdcba14dcb7e130240fba8
ansuz <ansuz@transitiontech.ca>
2021-04-15 14:35:01 +0530

    add a 'BETA' prefix to the calendar title

--- www/calendar/inner.js
+++ www/calendar/inner.js
@@ -52,7 +52,7 @@ define([
     var metadataMgr;
     var sframeChan;
 
-Messages.calendar = "Calendar"; // XXX
+Messages.calendar = "BETA Calendar"; // XXX
 Messages.calendar_default = "My calendar"; // XXX
 Messages.calendar_new = "New calendar"; // XXX
 Messages.calendar_day = "Day";
