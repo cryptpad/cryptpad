38370328ddf4fd77bc28e4023e9b0a9163fddc22
ansuz <ansuz@transitiontech.ca>
2021-09-07 17:06:00 +0530

    lint compliance

--- www/form/inner.js
+++ www/form/inner.js
@@ -2813,7 +2813,7 @@ define([
         $(logo).click(function () {
             APP.framework._.sfCommon.gotoURL('/');
         });
-        var footer = h('div.cp-form-view-footer', [logo])
+        var footer = h('div.cp-form-view-footer', [logo]);
         return footer;
     };
 
