a76fc3a4271bd84d1144aca3e764da97f8fc46f5
yflory <yann.flory@xwiki.com>
2021-10-04 16:09:42 +0200

    Update sdkjs

--- www/common/onlyoffice/v4/sdkjs/slide/themes/themes.js
+++ www/common/onlyoffice/v4/sdkjs/slide/themes/themes.js
@@ -1,1 +1,1 @@
-AscCommon.g_defaultThemes = ["Blank","Basic","Classic","Official","Green leaf","Lines","Office","Safari","Dotted","Corner","Turtle","XWiki"];
+AscCommon.g_defaultThemes = ["Blank","Basic","Classic","Official","Green leaf","Lines","Office","Safari","Dotted","Corner","Turtle"];
