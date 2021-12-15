41108bd92a10437f5c40887c21ff32a8d3e8aa36
ansuz <git@ansuz.xyz>
2021-12-15 12:44:41 +0530

    ignore onlyoffice v5 when linting translations

--- scripts/translations/unused-translations.js
+++ scripts/translations/unused-translations.js
@@ -31,6 +31,7 @@ var grep = function (pattern, cb) {
         'www/common/onlyoffice/v1/*',
         'www/common/onlyoffice/v2b*',
         'www/common/onlyoffice/v4*',
+        'www/common/onlyoffice/v5*',
         'www/common/onlyoffice/x2t/*',
         //'www/common/onlyoffice/build/*',
         'www/lib/*',
