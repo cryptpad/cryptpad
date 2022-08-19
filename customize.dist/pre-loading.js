06a8e7c866d6653fd8fe1dab52ae678dd5aa4aa6
Maxime Cesson <cessonmaxi@cy-tech.fr>
2022-08-19 16:39:20 +0200

    Improve transition from outer placeholder to inner one

--- customize.dist/pre-loading.js
+++ customize.dist/pre-loading.js
@@ -15,8 +15,26 @@ elem.innerHTML = [
     '</div>'
 ].join('');
 
+var req;
+try {
+    req = JSON.parse(decodeURIComponent(window.location.hash.substring(1)));
+    }
+} catch (e) {}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.body.appendChild(elem);
+    window.CP_preloadingTime = +new Date();
+
+    // soft transition between inner and outer placeholders
+    if (req && req.time && (+new Date() - req.time > 2000)) {
+        var logo = document.querySelector('.placeholder-logo-container');
+        var message = document.querySelector('.placeholder-message-container');
+        logo.style.opacity = 100;
+        message.style.opacity = 100;
+        logo.style.animation = 'none';
+        message.style.animation = 'none';
+    }
+
     // fallback if CSS animations not available
     setTimeout(() => {
         try {
