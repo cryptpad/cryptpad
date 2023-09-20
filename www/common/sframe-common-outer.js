cryptpad/cryptpad

commit b78ec996ec91fc7d137379988f670881de8541bc

Deduplicate code for iframe title

www/common/sframe-common-outer.js
@@ -73,7 +73,6 @@ define([
             ApiConfig.httpSafeOrigin + (pathname || window.location.pathname) + 'inner.html?' +
                 requireConfig.urlArgs + '#' + encodeURIComponent(JSON.stringify(req)));
         $i.attr('allowfullscreen', 'true');
-        // $i.attr('title', 'iframe'); // XXX to be fixed
         $('iframe-placeholder').after($i).remove();
 
         // This is a cheap trick to avoid loading sframe-channel in parallel with the
@@ -1230,6 +1229,8 @@ define([
             };
             addCommonRpc(sframeChan, isSafe);
 
+            var SecureModal = {};
+
             var currentTitle;
             var currentTabTitle;
             var titleSuffix = (Utils.Util.find(Utils, ['Instance','name','default']) || '').trim();
@@ -1237,12 +1238,16 @@ define([
                 titleSuffix = window.location.hostname;
             }
             var setDocumentTitle = function () {
+                var newTitle;
                 if (!currentTabTitle) {
-                    document.title = currentTitle || 'CryptPad';
-                    return;
+                    newTitle = currentTitle || 'CryptPad';
+                } else {
+                    var title = currentTabTitle.replace(/\{title\}/g, currentTitle || 'CryptPad');
+                    newTitle = title + ' - ' + titleSuffix;
                 }
-                var title = currentTabTitle.replace(/\{title\}/g, currentTitle || 'CryptPad');
-                document.title = title + ' - ' + titleSuffix;
+                document.title = newTitle;
+                sframeChan.event('EV_IFRAME_TITLE', newTitle);
+                if (SecureModal.modal) { SecureModal.modal.setTitle(newTitle); }
             };
 
             var setPadTitle = function (data, cb) {
@@ -1513,7 +1518,6 @@ define([
             });
 
             // Secure modal
-            var SecureModal = {};
             // Create or display the iframe and modal
             var getPropChannels = function () {
                 var channels = {};
@@ -1558,8 +1562,12 @@ define([
                 if (!cfg.hidden) {
                     SecureModal.modal.refresh(cfg, function () {
                         SecureModal.$iframe.show();
+                        setDocumentTitle();
                     });
                 } else {
+                    SecureModal.modal.refresh(cfg, function () {
+                        setDocumentTitle();
+                    });
                     SecureModal.$iframe.hide();
                     return;
                 }

www/secureiframe/main.js
@@ -187,8 +187,12 @@ define([
             sframeChan.event('EV_REFRESH', data);
             cb();
         };
+        var setTitle = function (title) {
+            sframeChan.event('EV_IFRAME_TITLE', title);
+        };
         return {
-            refresh: refresh
+            refresh: refresh,
+            setTitle: setTitle
         };
     };
     return {

www/admin/inner.js
@@ -45,7 +45,6 @@ define([
     Flatpickr
     )
 {
-    document.title = Messages.adminPage;
     var APP = {
         'instanceStatus': {}
     };

www/calendar/inner.html
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html class="cp-app-noscroll">
 <head>
-    <title></title>
     <meta content="text/html; charset=utf-8" http-equiv="content-type"/>
     <script src="/customize/pre-loading.js?ver=1.1"></script>
     <link href="/customize/src/pre-loading.css?ver=1.0" rel="stylesheet" type="text/css">

www/calendar/inner.js
@@ -54,7 +54,6 @@ define([
     Share, Access, Properties
     )
 {
-    document.title = Messages.calendar;
     var SaveAs = window.saveAs;
     var APP = window.APP = {
         calendars: {}

www/code/inner.js
@@ -62,9 +62,6 @@ define([
     Messages,
     CMeditor)
 {
-    Messages.application = "Application"; // XXX
-    Messages.cryptpad_title = "CryptPad"; // XXX
-    document.title = Messages.type.code + " " + Messages.application + " - " + Messages.cryptpad_title;
     window.CodeMirror = CMeditor;
 
     var MEDIA_TAG_MODES = Object.freeze([

www/common/drive-ui.js
@@ -2695,7 +2695,7 @@ define([
                 gridIcon,
                 listIcon
             ]));
-            $button.attr('aria-label', 'View Mode Button');
+            $button.attr('aria-label', 'View Mode Button'); // XXX missing key?
             var $gridIcon = $(gridIcon);
             var $listIcon = $(listIcon);
             var showMode = function (mode) {

www/common/onlyoffice/inner.js
@@ -662,23 +662,17 @@ define([
 
         var loadInitDocument = function (type, useNewDefault) {
             var newText;
-            Messages.application = "Application"; // XXX
-            Messages.cryptpad_title = "CryptPad"; // XXX
             switch (type) {
                 case 'sheet' :
-                    document.title = Messages.type.sheet + " " + Messages.application + " - " + Messages.cryptpad_title;
                     newText = EmptyCell(useNewDefault);
                     break;
                 case 'doc':
-                    document.title = Messages.type.doc + " " + Messages.application + " - " + Messages.cryptpad_title;
                     newText = EmptyDoc();
                     break;
                 case 'presentation':
-                    document.title = Messages.type.presentation + " " + Messages.application + " - " + Messages.cryptpad_title;
                     newText = EmptySlide();
                     break;
                 default:
-                    document.title = Messages.application + " - " + Messages.cryptpad_title;
                     newText = '';
             }
             return new Blob([newText], {type: 'text/plain'});

commit 40415ce99bc237921fd8583aaa66cd35afa73b96

Fix issue introduced with b78ec996

www/common/sframe-common-outer.js
@@ -1559,15 +1559,12 @@ define([
                     SecureModal.$iframe = $('<iframe>', {id: 'sbox-secure-iframe'}).appendTo($('body'));
                     SecureModal.modal = SecureIframe.create(config);
                 }
+                setDocumentTitle();
                 if (!cfg.hidden) {
                     SecureModal.modal.refresh(cfg, function () {
                         SecureModal.$iframe.show();
-                        setDocumentTitle();
                     });
                 } else {
-                    SecureModal.modal.refresh(cfg, function () {
-                        setDocumentTitle();
-                    });
                     SecureModal.$iframe.hide();
                     return;
                 }

www/secureiframe/main.js
@@ -4,11 +4,12 @@ define([
     '/api/config',
     'jquery',
     '/common/requireconfig.js',
+    '/common/common-util.js',
     '/customize/messages.js',
-], function (nThen, ApiConfig, $, RequireConfig, Messages) {
+], function (nThen, ApiConfig, $, RequireConfig, Util, Messages) {
     var requireConfig = RequireConfig();
 
-    var ready = false;
+    var readyEvt = Util.mkEvent(true);
 
     var create = function (config) {
         // Loaded in load #2
@@ -169,27 +170,20 @@ define([
                 });
 
                 sframeChan.onReady(function () {
-                    if (ready === true) { return; }
-                    if (typeof ready === "function") {
-                        ready();
-                    }
-                    ready = true;
+                    readyEvt.fire();
                 });
             });
         });
         var refresh = function (data, cb) {
-            if (!ready) {
-                ready = function () {
-                    refresh(data, cb);
-                };
-                return;
-            }
-            sframeChan.event('EV_REFRESH', data);
-            cb();
+            readyEvt.reg(() => {
+                sframeChan.event('EV_REFRESH', data);
+                cb();
+            });
         };
         var setTitle = function (title) {
-            if (!sframeChan) { return; }
-            sframeChan.event('EV_IFRAME_TITLE', title);
+            readyEvt.reg(() => {
+                sframeChan.event('EV_IFRAME_TITLE', title);
+            });
         };
         return {
             refresh: refresh,