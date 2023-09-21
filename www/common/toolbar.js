fa6e2b8479a2198d08d87f6292ef8f926ef0cdb7
David Benque <david.benque@xwiki.com>
2023-09-21 13:30:45 +0100

    Remove unnecessary translation keys

--- www/common/toolbar.js
+++ www/common/toolbar.js
@@ -862,12 +862,11 @@ MessengerUI, Messages, Pages) {
 
         var href = toMain ? origin+'/index.html' : origin+'/drive/';
         var buttonTitle = toMain ? Messages.header_homeTitle : Messages.header_logoTitle;
-        Messages.homeButton = "Home button"; // XXX
         var $aTag = $('<a>', {
             href: href,
             title: buttonTitle,
             'class': "cp-toolbar-link-logo",
-            'aria-label': Messages.homeButton,
+            'aria-label': buttonTitle,
         }).append(UI.getIcon(privateData.app));
 
         var onClick = function (e) {
@@ -1123,8 +1122,7 @@ MessengerUI, Messages, Pages) {
         $button.attr('title', Messages.notificationsPage);
         $button.addClass('fa fa-bell-o cp-notifications-bell');
         $button.addClass('fa fa-bell-o cp-notifications-bell');
-        Messages.notificationBell = "Notification Bell"; // XXX
-        $button.attr('aria-label', Messages.notificationBell);
+        $button.attr('aria-label', Messages.notificationsPage);
         var $n = $button.find('.cp-dropdown-button-title').hide();
         var $empty = $(div).find('.cp-notifications-empty');
 
