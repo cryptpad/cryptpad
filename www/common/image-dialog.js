049fa08250eadcaddbc560557b92674966451cb0
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-11-08 15:38:52 +0100

    WIP build cryptpad: urls for images

--- www/common/image-dialog.js
+++ www/common/image-dialog.js
@@ -3,17 +3,64 @@
 define([
     '/common/common-util.js',
     '/file/file-crypto.js',
+    '/common/common-hash.js',
 ], function(
     Util,
-    FileCrypto
+    FileCrypto,
+    Hash
 ) {
+
+    const getCryptPadUrl = function(src, key, type) {
+        const url = new URL(src);
+        const params = new URLSearchParams();
+        params.set('type', type);
+        url.search = params.toString();
+        url.protocol = url.protocol === 'https:' ? 'cryptpads:' : 'cryptpad:';
+        url.hash = key;
+        return url.href;
+    };
+
+    const parseCryptPadUrl = function(href) {
+        const url = new URL(href);
+        url.protocol = url.protocol === 'cryptpads:' ? 'https:' : 'http:';
+        const key = url.hash.substring(1);  // remove leading '#'
+        const type = url.searchParams.get('type');
+        url.search = '';
+        url.hash = '';
+        return { src: url.href, key, type };
+    }
 
     const setBlobType = (blob, mimeType) => {
         const fixedBlob = new Blob([blob], {type: mimeType});
         return fixedBlob;
     };
 
-    const getImage = function(common, data, callback) {
+    const loadImage = function(common, href) {
+        return new Promise((resolve, reject) => {
+            const { src, key, type } = parseCryptPadUrl(href);
+            Util.fetch(src, function (err, u8) {
+                if (err) {
+                    console.error(err);
+                    return void reject(err);
+                }
+                try {
+                    FileCrypto.decrypt(u8, nacl.util.decodeBase64(key), (err, res) => {
+                        if (err || !res.content) {
+                            console.error("Decrypting failed");
+                            return void reject(err);
+                        }
+
+                        resolve(setBlobType(res.content, type));
+                    });
+                } catch (e) {
+                    console.error(e);
+                    reject(err);
+                }
+            }, void 0, common.getCache());
+        });
+    };
+
+    const getImage = function(common, data, callback) {  // TODO remove
         Util.fetch(data.src, function (err, u8) {
             if (err) {
                 console.error(err);
@@ -35,29 +82,30 @@ define([
         }, void 0, common.getCache());
     };
 
-    return {
-        openImageDialog: function(common, integrationChannel, data, cb) {
-            if (integrationChannel) {
-                const ignoreFirstParam = (_, image) => cb(image);
-                integrationChannel.query('Q_INTEGRATION_ON_INSERT_IMAGE', data, ignoreFirstParam, {raw: true});
-                return;
+    const openImageDialog = function(common, integrationChannel, data, cb) {
+        if (integrationChannel) {
+            const ignoreFirstParam = (_, image) => cb(image);
+            integrationChannel.query('Q_INTEGRATION_ON_INSERT_IMAGE', data, ignoreFirstParam, {raw: true});
+            return;
+        }
+        common.openFilePicker({
+            types: ['file'],
+            where: ['root'],
+            filter: {
+                fileType: ['image/', 'application/x-drawio']
             }
-            common.openFilePicker({
-                types: ['file'],
-                where: ['root'],
-                filter: {
-                    fileType: ['image/', 'application/x-drawio']
-                }
-            }, (data) => {
-                getImage(common, data, function(blob) {
-                    common.setPadAttribute('atime', +new Date(), null, data.href);
-                    cb({
-                        name: data.name,
-                        fileType: data.fileType,
-                        blob: blob
-                    });
+        }, (data) => {
+            loadImage(common, getCryptPadUrl(data.src, data.key, data.fileType)).then((blob) => {
+                cb({
+                    name: data.name,
+                    fileType: data.fileType,
+                    blob: blob
                 });
             });
-        }
+        });
+    };
+
+    return {
+        openImageDialog,
     };
 });
