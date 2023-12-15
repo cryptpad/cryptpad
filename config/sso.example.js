01de1b9f47e48ce186cc5efab233d76e83262ba7
yflory <yann.flory@xwiki.com>
2023-12-15 16:06:25 +0100

    Update sso example conf

--- config/sso.example.js
+++ config/sso.example.js
@@ -1,3 +1,4 @@
+const fs = require('node:fs');
 module.exports = {
     // Enable SSO login on this instance
     enabled: false,
@@ -5,6 +6,8 @@ module.exports = {
     enforced: false,
     // Allow users to add an additional CryptPad password to their SSO account
     cpPassword: false,
+    // You can also force your SSO users to add a CryptPad password
+    forceCpPassword: false,
     // List of SSO providers
     list: [
     /*
@@ -15,6 +18,14 @@ module.exports = {
         client_id: "{your_client_id}",
         client_secret: "{your_client_secret}",
         jwt_alg: 'RS256' (optional)
+    }, {
+        name: 'samltest',
+        type: 'saml',
+        url: 'https://samltest.id/idp/profile/SAML2/Redirect/SSO',
+        issuer: 'your-cryptpad-issuer-id',
+        cert: String or fs.readFileSync("./your/cert/location", "utf-8"),
+        privateKey: fs.readFileSync("./your/private/key/location", "utf-8"),
+        signingCert: fs.readFileSync("./your/signing/cert/location", "utf-8"),
     }
     */
     ]
