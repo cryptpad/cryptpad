7af5ff8f0681536540bba829a0ce5bcd3ff41588
zuzanna-maria <zuzanna.ludzik@gmail.com>
2024-01-03 13:44:38 +0100

    added range array to utils + linting

--- www/form/inner.js
+++ www/form/inner.js
@@ -4109,20 +4109,22 @@ define([
                         idx = obj.idx;
                         _uid = Util.uid();
                         
+                        var itemKeys = Util.getKeysArray(content.form[uid].opts.items.length);
+                        var valueKeys = Util.getKeysArray(content.form[uid].opts.values.length);
                         var opts = type === 'multiradio' || type === 'multicheck' ? {
-                            items: [...Array(content.form[uid].opts.items.length).keys()].map(function (i) {
+                            items: itemKeys.map(function (i) {
                                 return {
                                     uid: Util.uid(),
                                     v: content.form[uid].opts.items[i].v
                                 };
                             }),
-                            values: [...Array(content.form[uid].opts.values.length).keys()].map(function (i) {
+                            values: valueKeys.map(function (i) {
                                 return {
                                     uid: Util.uid(),
                                     v: content.form[uid].opts.values[i].v
                                 };
                             })
-                        } : content.form[uid].opts
+                        } : content.form[uid].opts;
 
                         content.form[_uid] = {
                             q: content.form[uid].q,
