936ad5eab982bc300e7bbf69b9b129456f78faa1
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-02-22 08:03:20 +0100

    Remove unneeded files
    https://github.com/cryptpad/cryptpad/issues/1416

--- www/common/onlyoffice/build/update.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023 XWiki CryptPad Team <contact@cryptpad.org> and contributors
-#
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-cd web-apps
-git pull
-cd ../sdkjs
-git pull
-make clean
