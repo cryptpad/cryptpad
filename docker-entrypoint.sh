aed6bd7b2abb86b639de25e6ea1bf2efa3b310b1
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-02-26 08:37:25 +0100

    Remove OnlyOffice from docker image and install on startup
    https://github.com/cryptpad/cryptpad/issues/1416

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,13 +22,19 @@ if [ ! -f "$CPAD_CONF" ]; then
          eg: docker run -v /path/to/config.js:/cryptpad/config/config.js \n\
          #################################################################### \n"
 
-cp "$CPAD_HOME"/config/config.example.js "$CPAD_CONF"
+	cp "$CPAD_HOME"/config/config.example.js "$CPAD_CONF"
 
-sed -i  -e "s@\(httpUnsafeOrigin:\).*[^,]@\1 '$CPAD_MAIN_DOMAIN'@" \
-        -e "s@\(^ *\).*\(httpSafeOrigin:\).*[^,]@\1\2 '$CPAD_SANDBOX_DOMAIN'@" $CPAD_CONF
+	sed -i  -e "s@\(httpUnsafeOrigin:\).*[^,]@\1 '$CPAD_MAIN_DOMAIN'@" \
+        -e "s@\(^ *\).*\(httpSafeOrigin:\).*[^,]@\1\2 '$CPAD_SANDBOX_DOMAIN'@" "$CPAD_CONF"
 fi
 
 cd $CPAD_HOME
+eval "$(ssh-agent -s)"  # TODO remove this when repo is public
+ssh-add install-onlyoffice-docker.key
+mkdir -p ~/.ssh
+chmod 0700 ~/.ssh
+ssh-keyscan github.com > ~/.ssh/known_hosts
+./install-onlyoffice.sh -c -l # TODO run only when OnlyOffice is enabled
 npm run build
 
 exec "$@"
