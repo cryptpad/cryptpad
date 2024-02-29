bc87c971d7183d3f93e06b859af3e0681e3fbc27
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-02-29 12:34:03 +0100

    Use eslint-plugin-compat to check browser compability

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         'es2021': true,
         'node': true
     },
-    'extends': 'eslint:recommended',
+    'plugins': ['compat'],
+    'extends': ['eslint:recommended', 'plugin:compat/recommended'],
     "globals": {
         "define": "readonly",
     },
