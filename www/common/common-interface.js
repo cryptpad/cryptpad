34c8266e438c1903bcae6552ade9495fc202f666
daria <dariae9@yahoo.com>
2024-03-06 14:43:50 +0200

    delete unused code

--- www/common/common-interface.js
+++ www/common/common-interface.js
@@ -1464,56 +1464,6 @@ define([
         };
     };
 
-    /*  Given two jquery objects (a 'button' and a 'drawer')
-        add handlers to make it such that clicking the button
-        displays the drawer contents, and blurring the button
-        hides the drawer content. Used for toolbar buttons at the moment.
-    */
-    UI.createDrawer = function ($button, $content) {
-        $button.attr('aria-expanded', false);
-        $button.click(function () {
-            var topPos = $button[0].getBoundingClientRect().bottom;
-            $content.toggle();
-            $button.removeClass('cp-toolbar-button-active');
-            if ($content.is(':visible')) {
-                $button.addClass('cp-toolbar-button-active');
-                $content.focus();
-                var wh = $(window).height();
-                $content.css('max-height', Math.floor(wh - topPos - 1)+'px');
-            }
-        });
-
-        var observer = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                if (mutation.attributeName === "style") {
-                    var isVisible = $content.is(':visible');
-                    $button.attr('aria-expanded', isVisible);
-                }
-            });
-        });
-        observer.observe($content[0], { attributes: true });
-
-        var onBlur = function (e) {
-            if (e.relatedTarget) {
-                var $relatedTarget = $(e.relatedTarget);
-
-                if ($relatedTarget.is('.cp-toolbar-drawer-button')) { return; }
-                if ($relatedTarget.parents('.cp-toolbar-drawer-content').length) {
-                    $relatedTarget.blur(onBlur);
-                    return;
-                }
-            }
-            $button.removeClass('cp-toolbar-button-active');
-            $content.hide();
-        };
-        $content.blur(onBlur).appendTo($button);
-        $('body').keydown(function (e) {
-            if (e.which === 27) {
-                $content.blur();
-            }
-        });
-    };
-
 /*  QR code generation is synchronous once the library is loaded
     so this could be syncronous if we load the library separately. */
     UI.createQRCode = function (data, _cb) {
