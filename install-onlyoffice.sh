fb2ff4a40d572b6529245324c713f1183ec979bc
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-05-15 09:45:33 +0200

    Workarounds for missing OnlyOffice methods
    https://github.com/cryptpad/cryptpad/issues/1489

--- install-onlyoffice.sh
+++ install-onlyoffice.sh
@@ -6,7 +6,7 @@
 
 set -euo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 CONF_DIR=$SCRIPT_DIR/onlyoffice-conf
 BUILDS_DIR=$CONF_DIR/onlyoffice-builds.git
 OO_DIR=$SCRIPT_DIR/www/common/onlyoffice/dist
@@ -14,50 +14,50 @@ PROPS_FILE="$CONF_DIR"/onlyoffice.properties
 
 declare -A PROPS
 
-main () {
-	mkdir -p "$CONF_DIR"
+main() {
+    mkdir -p "$CONF_DIR"
 
-	load_props
+    load_props
 
-	parse_arguments "$@"
+    parse_arguments "$@"
 
-	ask_for_license
+    ask_for_license
 
-	# Remeber the 1st version that is installed. This will help us install only
-	# needed OnlyOffice versions in a later version of this script.
-	set_prop oldest_needed_version v1
+    # Remeber the 1st version that is installed. This will help us install only
+    # needed OnlyOffice versions in a later version of this script.
+    set_prop oldest_needed_version v1
 
-	mkdir -p "$OO_DIR"
-	install_version v1 4f370beb
-	install_version v2b d9da72fd
-	install_version v4 6ebc6938
-	install_version v5 88a356f0
-	install_version v6 abd8a309
-	install_version v7 3506b60c
-	install_x2t v7.3+1 ab0c05b0e4c81071acea83f0c6a8e75f5870c360ec4abc4af09105dd9b52264af9711ec0b7020e87095193ac9b6e20305e446f2321a541f743626a598e5318c1
+    mkdir -p "$OO_DIR"
+    install_version v1 4f370beb
+    install_version v2b d9da72fd
+    install_version v4 6ebc6938
+    install_version v5 88a356f0
+    install_version v6 abd8a309
+    install_version v7 ba82142f
 
-	rm -rf "$BUILDS_DIR"
-	if command -v rdfind &> /dev/null; then
-		rdfind -makehardlinks true -makeresultsfile false $OO_DIR/v*
-	fi
+    rm -rf "$BUILDS_DIR"
+    if command -v rdfind &>/dev/null; then
+        rdfind -makehardlinks true -makeresultsfile false $OO_DIR/v*
+    fi
 }
 
-load_props () {
-	if [ -e "$PROPS_FILE" ]; then
-		while IFS='=' read -r key value; do
-			PROPS["$key"]="$value"
-		done < "$PROPS_FILE"
-	fi
+load_props() {
+    if [ -e "$PROPS_FILE" ]; then
+        while IFS='=' read -r key value; do
+            PROPS["$key"]="$value"
+        done <"$PROPS_FILE"
+    fi
 }
 
-set_prop () {
-	PROPS["$1"]="$2"
+set_prop() {
+    PROPS["$1"]="$2"
 
-	for i in "${!PROPS[@]}"; do
-		echo "$i=${PROPS[$i]}"
-	done > "$PROPS_FILE"
+    for i in "${!PROPS[@]}"; do
+        echo "$i=${PROPS[$i]}"
+    done >"$PROPS_FILE"
 }
 
+<<<<<<< HEAD
 parse_arguments () {
 	while [[ $# -gt 0 ]]; do
 		case $1 in
@@ -75,25 +75,65 @@ parse_arguments () {
 				;;
 		esac
 	done
+||||||| parent of 1bc91b394 (Workarounds for missing OnlyOffice methods)
+parse_arguments () {
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+			-h|--help)
+				show_help
+				shift
+				;;
+			-a|--accept-license)
+				ACCEPT_LICENSE="1"
+				shift
+				;;
+			*)
+				show_help
+				shift
+				;;
+		esac
+	done 
+=======
+parse_arguments() {
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        -h | --help)
+            show_help
+            shift
+            ;;
+        -a | --accept-license)
+            ACCEPT_LICENSE="1"
+            shift
+            ;;
+        *)
+            show_help
+            shift
+            ;;
+        esac
+    done
+>>>>>>> 1bc91b394 (Workarounds for missing OnlyOffice methods)
 }
 
-ask_for_license () {
-	if [ ${ACCEPT_LICENSE+x} ] || [ "${PROPS[agree_license]:-no}" == yes ]; then
-		return
-	fi
+ask_for_license() {
+    if [ ${ACCEPT_LICENSE+x} ] || [ "${PROPS[agree_license]:-no}" == yes ]; then
+        return
+    fi
 
-	ensure_command_available curl
+    ensure_command_available curl
 
-	(echo -e "Please review the license of OnlyOffice:\n\n" ; curl https://raw.githubusercontent.com/ONLYOFFICE/web-apps/master/LICENSE.txt 2>/dev/null) | less
+    (
+        echo -e "Please review the license of OnlyOffice:\n\n"
+        curl https://raw.githubusercontent.com/ONLYOFFICE/web-apps/master/LICENSE.txt 2>/dev/null
+    ) | less
 
-	read -rp "Do you accept the license? (Y/N): " confirm \
-		&& [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+    read -rp "Do you accept the license? (Y/N): " confirm &&
+        [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-	set_prop "agree_license" yes
+    set_prop "agree_license" yes
 }
 
-show_help () {
-	cat << EOF
+show_help() {
+    cat <<EOF
 install-onlyoffice installs or upgrades OnlyOffice.
 
 NOTE: When you have rdfind installed, it will be used to save ~650MB of disk
@@ -108,48 +148,48 @@ OPTIONS:
             script. Read and accept this before using this option:
             https://github.com/ONLYOFFICE/web-apps/blob/master/LICENSE.txt
 EOF
-	exit 1
+    exit 1
 }
 
-ensure_oo_is_downloaded () {
-	ensure_command_available git
+ensure_oo_is_downloaded() {
+    ensure_command_available git
 
-	if ! [ -d "$BUILDS_DIR" ]; then
-		echo "Downloading OnlyOffice..."
-		git clone --bare https://github.com/cryptpad/onlyoffice-builds.git "$BUILDS_DIR"
-	fi
+    if ! [ -d "$BUILDS_DIR" ]; then
+        echo "Downloading OnlyOffice..."
+        git clone --bare https://github.com/cryptpad/onlyoffice-builds.git "$BUILDS_DIR"
+    fi
 }
 
-install_version () {
-	local DIR=$1
-	local COMMIT=$2
-	local FULL_DIR=$OO_DIR/$DIR
-	local LAST_DIR
-	LAST_DIR=$(pwd)
+install_version() {
+    local DIR=$1
+    local COMMIT=$2
+    local FULL_DIR=$OO_DIR/$DIR
+    local LAST_DIR
+    LAST_DIR=$(pwd)
 
-	if [ ! -e "$FULL_DIR"/.commit ] || [ "$(cat "$FULL_DIR"/.commit)" != "$COMMIT" ]; then
-		ensure_oo_is_downloaded
+    if [ ! -e "$FULL_DIR"/.commit ] || [ "$(cat "$FULL_DIR"/.commit)" != "$COMMIT" ]; then
+        ensure_oo_is_downloaded
 
-		rm -rf "$FULL_DIR"
+        rm -rf "$FULL_DIR"
 
-		cd "$BUILDS_DIR"
-		git worktree add "$FULL_DIR" "$COMMIT"
+        cd "$BUILDS_DIR"
+        git worktree add "$FULL_DIR" "$COMMIT"
 
-		cd "$LAST_DIR"
+        cd "$LAST_DIR"
 
-		echo "$COMMIT" > "$FULL_DIR"/.commit
+        echo "$COMMIT" >"$FULL_DIR"/.commit
 
-		echo "$DIR updated"
-	else
-		echo "$DIR was up to date"
-	fi
+        echo "$DIR updated"
+    else
+        echo "$DIR was up to date"
+    fi
 
-
-	if [ ${CLEAR+x} ]; then
-		rm -f "$FULL_DIR"/.git
-	fi
+    if [ ${CLEAR+x} ]; then
+        rm -f "$FULL_DIR"/.git
+    fi
 }
 
+<<<<<<< HEAD
 install_x2t () {
 	ensure_command_available curl
 	ensure_command_available sha512sum
@@ -190,6 +230,19 @@ ensure_command_available () {
 		echo "$1 needs to be installed to run this script"
 		exit 1
 	fi
+||||||| parent of 1bc91b394 (Workarounds for missing OnlyOffice methods)
+ensure_command_available () {
+	if ! command -v "$1" &> /dev/null; then
+		echo "$1 needs to be installed to run this script"
+		exit 1
+	fi
+=======
+ensure_command_available() {
+    if ! command -v "$1" &>/dev/null; then
+        echo "$1 needs to be installed to run this script"
+        exit 1
+    fi
+>>>>>>> 1bc91b394 (Workarounds for missing OnlyOffice methods)
 }
 
 main "$@"
