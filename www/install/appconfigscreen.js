4ec0e7e50e288b5a61cc923bd35311ef47cf2728
zuzanna-maria <zuzanna.ludzik@gmail.com>
2024-05-24 13:36:19 +0100

    add sendadmindecree

--- www/install/appconfigscreen.js
+++ www/install/appconfigscreen.js
@@ -1,3 +1,5 @@
+
+
 define([
     'jquery',
     '/common/toolbar.js',
@@ -48,20 +50,107 @@ define([
     ConfigScreen
 ) {
 
-var AppConfigScreen = {}
 
+    Messages.admin_appSelection = 'App configuration saved'
+    Messages.admin_appsTitle = "Choose your applications"
+    Messages.admin_appsHint = "Choose which apps are available to users on your instance."
+    Messages.admin_cat_apps = "Apps"
 
-AppConfigScreen.addConfigScreen = function (config) {
-        var LOADING = 'cp-loading';
-        config = config || {};
+    var APP = window.APP = {};
 
-        if ($('#' + LOADING).length) {
+    var Nacl = window.nacl;
+    var common;
+    var sFrameChan;
+    // SFCommon.create(waitFor(function(c) { APP.common = common = c; }));
+
+    // sFrameChan = common.getSframeChannel();
+    // sFrameChan.onReady(waitFor());
+
+    const blocks = Sidebar.blocks;
+    const grid = blocks.block([], 'cp-admin-customize-apps-grid');
+
+    const allApps = ['pad', 'code', 'kanban', 'slide', 'sheet', 'form', 'whiteboard', 'diagram'];
+    const availableApps = []
+        
+    function select(app) {
+        if (availableApps.indexOf(app) === -1) {
+            availableApps.push(app);
+            $(`#${app}-block`).attr('class', 'active-app') 
         } else {
-            ConfigScreen();
-        }
+            availableApps.splice(availableApps.indexOf(app), 1)
+            $(`#${app}-block`).attr('class', 'inactive-app')
+        } 
+    }
 
+    allApps.forEach(app => { 
+    // 'width: 50%; height: 80px; margin: 5px'
+    // width:200px;height:20px;float:left;border:1px solid red
+        let appBlock = h('div',  {style: 'width:50%;height:20px;float:left;border:1px solid red'}, {class: 'inactive-app', id: `${app}-block`}, app)
+        $(appBlock).addClass('cp-app-drive-element-grid')
+        $(appBlock).addClass('cp-app-drive-element-row')
+        $(appBlock).addClass('cp-app-drive-new-doc')
+
+
+        $(grid).append(appBlock);
+        $(appBlock).on('click', () => select(app))
+    }); 
+    
+
+    var save = blocks.activeButton('primary', '', Messages.settings_save, function (done) {
+        sFrameChan.query('Q_ADMIN_RPC', {
+            cmd: 'ADMIN_DECREE',
+            data: ['DISABLE_APPS', availableApps]
+        }, function (e, response) {
+            if (e || response.error) {
+                UI.warn(Messages.error);
+                $input.val('');
+                console.error(e, response);
+                done(false);
+                return;
+            }
+            flushCache();
+            done(true);
+            UI.log(Messages._getKey('ui_saved', [Messages.admin_appSelection]));
+        });
+    });
+    
+    
+    let form = blocks.form([
+        grid 
+    ], blocks.nav([save]));
+
+    return grid
+
+    var elem = document.createElement('div');
+    elem.setAttribute('id', 'cp-loading');
+
+    let frame = h('div.configscreen',  {style: 'width: 70%; height: 75%; background-color: white'}, form)
+
+    elem.append(frame)
+
+            built = true;
+        var intr;
+        var append = function () {
+            if (!document.body) { return; }
+            clearInterval(intr);
+            document.body.appendChild(elem);
+        };
+        intr = setInterval(append, 100);
+        append();
+
+
+    return function () {
+        built = true;
+        var intr;
+        var append = function () {
+            if (!document.body) { return; }
+            clearInterval(intr);
+            document.body.appendChild(elem);
+        };
+        intr = setInterval(append, 100);
+        append();
     };
 
-return AppConfigScreen
+
 });
 
--- www/install/main.js
+++ www/install/main.js
@@ -17,12 +17,18 @@ define([
     '/customize/pages.js',
     '/common/rpc.js',
     'appconfigscreen.js',
+    '/common/inner/sidebar-layout.js',
 
     'css!/components/components-font-awesome/css/font-awesome.min.css',
-], function ($, Login, Cryptpad, /*Test,*/ Cred, UI, Util, Realtime, Constants, Feedback, LocalStore, h, Pages, Rpc, AppConfigScreen) {
+], function ($, Login, Cryptpad, /*Test,*/ Cred, UI, Util, Realtime, Constants, Feedback, LocalStore, h, Pages, Rpc, AppConfigScreen, Sidebar) {
     if (window.top !== window) { return; }
     var Messages = Cryptpad.Messages;
     $(function () {
+
+        Messages.admin_appSelection = 'App configuration saved'
+        Messages.admin_appsTitle = "Choose your applications"
+        Messages.admin_appsHint = "Choose which apps are available to users on your instance."
+        Messages.admin_cat_apps = "Apps"
         // if (LocalStore.isLoggedIn()) {
         //     // already logged in, redirect to drive
         //     document.location.href = '/drive/';
@@ -56,9 +62,67 @@ define([
             }
         }
 
-        var registerClick = function () {
+        var bloop = function (sendAdminDecree) {
 
-            AppConfigScreen.addConfigScreen()
+            const blocks = Sidebar.blocks;
+            var grid = AppConfigScreen
+            
+            var save = blocks.activeButton('primary', '', Messages.settings_save, function (done) {
+                console.log('hello!');
+                console.log(sendAdminDecree)
+                sendAdminDecree()
+                UI.log('Messages._getKey(, [Messages.admin_appSelection])');
+            });
+
+            
+            let form = blocks.form([
+                grid 
+            ], blocks.nav([save]));
+
+            var elem = document.createElement('div');
+            elem.setAttribute('id', 'cp-loading');
+            let frame = h('div.configscreen',  {style: 'width: 70%; height: 75%; background-color: white'}, form)
+            elem.append(frame)
+
+            built = true;
+        var intr;
+        var append = function () {
+            if (!document.body) { return; }
+            clearInterval(intr);
+            document.body.appendChild(elem);
+        };
+        intr = setInterval(append, 100);
+        append();
+
+
+            // return function () {
+            //     built = true;
+            //     var intr;
+            //     var append = function () {
+            //         if (!document.body) { return; }
+            //         clearInterval(intr);
+            //         document.body.appendChild(elem);
+            //     };
+            //     intr = setInterval(append, 100);
+            //     append();
+            // };
+        }
+
+        var registerClick = function () {
+        // AppConfigScreen
+        // console.log(AppConfigScreen)
+            // var sendAdminDecree = 'bleeop'
+                            let sendAdminDecree = function (command, data, callback) {
+                            console.log('belp')
+                    // var params = ['ADMIN_DECREE', [command, data]];  
+                    // rpc.send('ADMIN', params, callback)
+                };
+            bloop(sendAdminDecree)
+
+            // AppConfigScreen
+            // console.log(AppConfigScreen)
+            // console.log(AppConfigScreen)
+
         };
 
         $register.click(registerClick);
