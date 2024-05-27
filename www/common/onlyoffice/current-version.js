0309a2462eb9aca299fead1b8e46510b487d9232
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-05-27 13:45:44 +0200

    Update to version 7

--- www/common/onlyoffice/current-version.js
+++ www/common/onlyoffice/current-version.js
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 define(() => {
-    const version = 6;
+    const version = 7;
     return {
         currentVersionNumber: version,
         currentVersion: 'v' + version
