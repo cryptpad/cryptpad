9cc0b231c846369c428d4ef0bee3284f68e590e1
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-09-23 14:38:36 +0200

    add more buttons

--- www/tiptap/inner.js
+++ www/tiptap/inner.js
@@ -26,6 +26,155 @@ define([
                 icon: 'fa-bold',
                 run: () => editor.chain().focus().toggleBold().run(),
             },
+            {
+                icon: 'fa-italic',
+                run: () => editor.chain().focus().toggleItalic().run(),
+            },
+            {
+                icon: 'fa-strikethrough',
+                run: () => editor.chain().focus().toggleStrike().run(),
+            },
+            {
+                icon: 'fa-code',
+                run: () => editor.chain().focus().toggleCode().run(),
+            },
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleCode().run()}
+            //           disabled={
+            //             !editor.can()
+            //               .chain()
+            //               .focus()
+            //               .toggleCode()
+            //               .run()
+            //           }
+            //           className={editor.isActive('code') ? 'is-active' : ''}
+            //         >
+            //           Code
+            //         </button>
+            //         <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+            //           Clear marks
+            //         </button>
+            //         <button onClick={() => editor.chain().focus().clearNodes().run()}>
+            //           Clear nodes
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().setParagraph().run()}
+            //           className={editor.isActive('paragraph') ? 'is-active' : ''}
+            //         >
+            //           Paragraph
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+            //           className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+            //         >
+            //           H1
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+            //           className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+            //         >
+            //           H2
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+            //           className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+            //         >
+            //           H3
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+            //           className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+            //         >
+            //           H4
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+            //           className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+            //         >
+            //           H5
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+            //           className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+            //         >
+            //           H6
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleBulletList().run()}
+            //           className={editor.isActive('bulletList') ? 'is-active' : ''}
+            //         >
+            //           Bullet list
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            //           className={editor.isActive('orderedList') ? 'is-active' : ''}
+            //         >
+            //           Ordered list
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+            //           className={editor.isActive('codeBlock') ? 'is-active' : ''}
+            //         >
+            //           Code block
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().toggleBlockquote().run()}
+            //           className={editor.isActive('blockquote') ? 'is-active' : ''}
+            //         >
+            //           Blockquote
+            //         </button>
+            //         <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+            //           Horizontal rule
+            //         </button>
+            //         <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+            //           Hard break
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().undo().run()}
+            //           disabled={
+            //             !editor.can()
+            //               .chain()
+            //               .focus()
+            //               .undo()
+            //               .run()
+            //           }
+            //         >
+            //           Undo
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().redo().run()}
+            //           disabled={
+            //             !editor.can()
+            //               .chain()
+            //               .focus()
+            //               .redo()
+            //               .run()
+            //           }
+            //         >
+            //           Redo
+            //         </button>
+            //         <button
+            //           onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+            //           className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+            //         >
+            //           Purple
+            //         </button>
+            //       </div>
+            //     </div>
+            //   )
+            // }
+
+            // const extensions = [
+            //   Color.configure({ types: [TextStyle.name, ListItem.name] }),
+            //   TextStyle.configure({ types: [ListItem.name] }),
+            //   StarterKit.configure({
+            //     bulletList: {
+            //       keepMarks: true,
+            //       keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+            //     },
+            //     orderedList: {
+            //       keepMarks: true,
+            //       keepAttributes: false,
         ];
 
         // var onClick = function () {
@@ -114,7 +263,7 @@ define([
             const element = document.querySelector('.cp-tiptap-element');
             const editor = Tiptap.start(element);
 
-            createToolbar($('#cp-tiptap-toolbar'), editoR);
+            createToolbar($('#cp-tiptap-toolbar'), editor);
         });
 
         // Start the framework
