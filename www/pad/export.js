1354b0a238bf0a866d3c0823c55951a93f233d5e
zuzanna-maria <zuzanna.ludzik@gmail.com>
2024-12-09 13:08:36 +0100

    Formatting accounts for bold, italic, underlined text and blank cells

--- www/pad/export.js
+++ www/pad/export.js
@@ -111,19 +111,29 @@ define([
                                 for (var i =0; i < rowLength; i++) {
                                     var cell = rowContent[i];
                                     var cellContent = Array.from(cell.childNodes);
-                                    cellContent.pop();
                                     if (cellContent.length > 1) {
                                         var cellString = '';
-                                        cellContent.forEach(function(string) {
+                                        cellContent.forEach(function(string) {  
+                                        var stringContent = string.childNodes.length ? string.innerHTML : string.textContent
                                             if (string.nodeType === 3) {
-                                                cellString += string.textContent;
+                                                cellString += stringContent;
                                             } else if (string.nodeName === "BR") {
                                                 cellString += '<br>';
+                                            }  else if (string.nodeName === "EM") {
+                                                cellString += '<i>' + stringContent + '</i>';
+                                            } else if (string.nodeName === "STRONG") {
+                                                cellString += '<b>' + stringContent + '</b>';
+                                            } else if (string.nodeName === "U") {
+                                                cellString += '<u>' + stringContent + '</u>';
+                                            } else if (string.nodeName === "S") {
+                                                cellString += '~' + stringContent + '~';
                                             }
                                         });
                                         row += cellString + ' |';
-                                    } else {
-                                        row += cellContent[0].textContent + ' |';
+                                    } else if (cellContent[0].nodeName === "BR") {    
+                                        row += '|  |';
+                                    } else {    
+                                        row += cellContent[0].innerHTML + ' |';
                                     }
                                 }
                                 var newRow = row.concat('\n');
@@ -137,10 +147,15 @@ define([
                         });
                     return table;
                 }}).addRule('strikethrough', {
-                    filter: ['s', 'del', 'strike'],
-                    replacement: function (content) {
-                        return '~' + content + '~';
-                    }
+                        filter: ['s', 'del', 'strike'],
+                        replacement: function (content) {
+                            return '~' + content + '~';
+                        }
+                }).addRule('strikethrough', {
+                        filter: ['u'],
+                        replacement: function (content) {
+                            return '<u>' + content + '</u>';
+                        }
                 })
                 .turndown(toExport);
                 var mdBlob = new Blob([md], {
