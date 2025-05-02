2c029a0ce7f317cfadff1fa3415486274fced48f
yflory <yann.flory@xwiki.com>
2025-05-02 10:52:17 +0200

    Use unminified build in dev mode

--- www/common/store-interface.js
+++ www/common/store-interface.js
@@ -5,6 +5,7 @@
 (() => {
 const factory = function () {
     let USE_MIN = true;
+    if (localStorage.CryptPad_dev === "1") { USE_MIN = false; }
 
     let path = '/common/worker.bundle.js?';
     if (USE_MIN) { path = '/common/worker.bundle.min.js?'; }
