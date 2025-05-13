9923382550d924580b93753e9e312a3a304c3730
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2025-05-13 16:06:45 +0200

    Revert "Modify OO install script so you can selectively install versions"
    This reverts commit 80ef7f61cb96f0385a2facd0e60f0a49775ce595.

--- install-onlyoffice.sh
+++ install-onlyoffice.sh
@@ -37,36 +37,6 @@ main() {
     install_version v8 v8.3.3.23+0 d3752926a68a487c5f855651a8966c04b320277a0dd2dfc3a4c1537b8d925cf59ec93a15f97664cbdb8867c32a94e105798b28751e0ef49521bc72c323b634a8
     install_x2t v7.3+1 ab0c05b0e4c81071acea83f0c6a8e75f5870c360ec4abc4af09105dd9b52264af9711ec0b7020e87095193ac9b6e20305e446f2321a541f743626a598e5318c1
 
-    echo "Do you want to install all available OnlyOffice versions? (Y/N)"
-    read -r install_all
-
-    declare -a selected_versions
-
-    if [[ "$install_all" =~ ^[Yy] ]]; then
-        selected_versions=(v1 v2b v4 v5 v6 v7 v8 x2t)
-    else
-        echo "Select which versions to install (space-separated), options are:"
-        echo "  v1 v2b v4 v5 v6 v7 v8 x2t"
-        read -ra selected_versions
-    fi
-
-    for version in "${selected_versions[@]}"; do
-        case $version in
-            v1)  set_prop oldest_needed_version v1; install_old_version v1 4f370beb ;;
-            v2b) install_old_version v2b d9da72fd ;;
-            v4)  install_old_version v4 6ebc6938 ;;
-            v5)  install_old_version v5 88a356f0 ;;
-            v6)  install_old_version v6 abd8a309 ;;
-            v7)  install_version v7 v7.3.3.60+10 be3c926d534b0c77aa1ed83b170a38b9e56ebbbfd73a1a968f523d63c5ed69ecdb063fad08740812de82c1e79af7309faf67ffdd81b45451adce539192f3414c ;;
-            v8)  install_version v8 v8.3.0.83+3 fbf77671ea8b960b6c65fdad025980894dfc9ed0b79d9540951f56c79871d0bde3361f29a35b9653764f7802ec74df01601b6c9961e0f3118988422667563470 ;;
-            x2t) install_x2t v7.3+1 ab0c05b0e4c81071acea83f0c6a8e75f5870c360ec4abc4af09105dd9b52264af9711ec0b7020e87095193ac9b6e20305e446f2321a541f743626a598e5318c1 ;;
-            *)
-                echo "Unknown version: $version"
-                exit 1
-                ;;
-        esac
-    done
-
     rm -rf "$BUILDS_DIR"
 
     if [ "${RDFIND+x}" != "x" ]; then
