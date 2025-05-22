480867d3bf85ff4f218ef220673abc62b99cd853
daria <95225431+dariiing@users.noreply.github.com>
2025-05-22 12:51:13 -0700

    change aria-live

--- www/common/common-interface.js
+++ www/common/common-interface.js
@@ -1077,7 +1077,7 @@ define([
             $loading.css('display', '');
             $loading.removeClass('cp-loading-hidden');
             $loading.removeClass('cp-loading-transparent');
-            $loading.attr('aria-live','assertive');
+            $loading.attr('aria-live','polite');
             if (config.newProgress) {
                 var progress = h('div.cp-loading-progress', [
                     h('p.cp-loading-progress-list'),
