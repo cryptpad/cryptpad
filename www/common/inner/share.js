cbe7def797290051fe056a2b4c6248e1afef893b
Zuzanna <zuzanna.ludzik@gmail.com>
2025-06-26 15:49:01 +0200

    Can share in auditor mode after saving auditor link to drive

--- www/common/inner/share.js
+++ www/common/inner/share.js
@@ -631,6 +631,7 @@ define([
                             labelEdit, false, { mark: {tabindex:0} }),
             auditor]),
             burnAfterReading,
+
         ]);
 
         // Burn after reading
@@ -715,14 +716,18 @@ define([
             $rights.find('#cp-share-present').removeAttr('checked').attr('disabled', true);
             $rights.find('#cp-share-editable-true').removeAttr('checked').attr('disabled', true);
         } else if (!hashes.editHash) {
-            $rights.find('#cp-share-editable-false').attr('checked', true);
+            if (hash.indexOf('auditor') !== -1) {
+              $rights.find('#cp-share-editable-false').attr('checked', false).attr('disabled', true);
+            } else {
+                $rights.find('#cp-share-editable-false').attr('checked', true);
+            }
             $rights.find('#cp-share-editable-true').removeAttr('checked').attr('disabled', true);
         } else if (!hashes.viewHash) {
             $rights.find('#cp-share-editable-false').removeAttr('checked').attr('disabled', true);
             $rights.find('#cp-share-present').removeAttr('checked').attr('disabled', true);
             $rights.find('#cp-share-editable-true').attr('checked', true);
         }
-        if (isForm && !opts.auditorHash) {
+        if (isForm && !opts.auditorHash && hash.indexOf('auditor') === -1) {
             $rights.find('#cp-share-form').removeAttr('checked').attr('disabled', true);
         }
 
@@ -772,7 +777,12 @@ define([
                 $rights.find('#cp-share-editable-true').prop('checked', false);
                 $rights.find('#cp-share-present').prop('checked', true);
             } else if ((val.edit === false && hashes.viewHash) || !hashes.editHash) {
-                $rights.find('#cp-share-editable-false').prop('checked', true);
+                if (hash.indexOf('auditor') !== -1) {
+                    $rights.find('#cp-share-editable-false').prop('checked', false);
+                    $rights.find('#cp-share-form').prop('checked', true);
+                } else {
+                    $rights.find('#cp-share-editable-false').prop('checked', true);
+                }
                 $rights.find('#cp-share-editable-true').prop('checked', false);
                 $rights.find('#cp-share-present').prop('checked', false);
             } else {
