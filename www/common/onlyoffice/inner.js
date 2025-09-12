a38ac1e19a39b73921a9008f54989eb8fcf7bec4
Zuzanna <zuzanna.ludzik@gmail.com>
2025-09-12 12:59:30 +0200

    WIP

--- www/common/onlyoffice/inner.js
+++ www/common/onlyoffice/inner.js
@@ -587,7 +587,7 @@ define([
             blob.name = title || (metadataMgr.getMetadataLazy().title || file.doc) + '.' + file.type;
             var data = {
                 hash: (APP.history || APP.template) ? ooChannel.historyLastHash : ooChannel.lastHash,
-                index: (APP.history || APP.template) ? ooChannel.currentIndex : ooChannel.cpIndex
+                index:  ooChannel.cpIndex
             };
             fixSheets();
 
@@ -1440,7 +1440,7 @@ define([
                 return;
             }
 
-            debug(obj, 'toOOClient');
+            // debug(obj, 'toOOClient');
             APP.docEditor.sendMessageToOO(obj);
             if (obj && obj.type === "saveChanges") {
                 evIntegrationSave.fire();
@@ -1448,7 +1448,7 @@ define([
         };
 
         const fromOOHandler = function (obj) {
-            debug(obj, 'fromOOClient');
+            // debug(obj, 'fromOOClient');
             switch (obj.type) {
                 case "auth":
                     // Handled by onlyoffice-editor now
@@ -3140,7 +3140,8 @@ Uncaught TypeError: Cannot read property 'calculatedType' of null
                     loadCp(cp, true);
                 };
                 var onPatchBack = function (cp, msgs) {
-                    msgsFormatted = []
+                    if (msgs) {
+                        msgsFormatted = []
                     msgs.forEach(function(msg) {
                         var parsedMsg = JSON.parse(msg.msg);
     
@@ -3158,6 +3159,10 @@ Uncaught TypeError: Cannot read property 'calculatedType' of null
                     setTimeout(() => {
                     loadCp(cp, true);
                     }, 100);
+                    } else {
+                        loadCp(cp)
+                    }
+                    
                     
                 };
                 var setHistoryMode = function (bool) {
