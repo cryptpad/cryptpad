7e136abaa98a2846d3513e63dadbcf1c1c137e8e
Wolfgang Ginolas <wolfgang@cryptpad.org>
2025-11-14 15:41:47 +0100

    removing cursor

--- www/common/onlyoffice/inner.js
+++ www/common/onlyoffice/inner.js
@@ -562,20 +562,7 @@ define([
             }
             myUniqueOOId = undefined;
             setMyId();
-            var editor = getEditor();
-            if (editor) {
-                var app = common.getMetadataMgr().getPrivateData().ooType;
-                var d;
-                if (app === 'doc') {
-                    d = editor.GetDocument() ? editor.GetDocument().Document : undefined;
-                } else if (app === 'presentation') {
-                    d = editor.GetPresentation().Presentation;
-                }
-                if (d) {
-                    APP.oldCursor = d.GetSelectionState();
-                }
-            }
-            if (APP.docEditor && APP.docEditor.destroyEditor() ) { APP.docEditor.destroyEditor(); } // Kill the old editor
+            if (APP.docEditor) { APP.docEditor.destroyEditor(); } // Kill the old editor
             $('iframe[name="frameEditor"]').after(h('div#cp-app-oo-placeholder-a')).remove();
             ooLoaded = false;
             oldLocks = {};
@@ -2059,20 +2046,6 @@ define([
                     getEditor().asc_setDefaultLanguage(l);
                 }
 
-                if (APP.oldCursor) {
-                    var app = common.getMetadataMgr().getPrivateData().ooType;
-                    var d;
-                    if (app === 'doc') {
-                        d = getEditor().GetDocument().Document;
-                    } else if (app === 'presentation') {
-                        d = getEditor().GetPresentation().Presentation;
-                    }
-                    if (d) {
-                        d.SetSelectionState(APP.oldCursor);
-                        d.UpdateSelection();
-                    }
-                    delete APP.oldCursor;
-                }
                 if (integrationChannel) {
                     APP.onDocumentUnlock = () => {
                         integrationChannel.event('EV_INTEGRATION_READY');
