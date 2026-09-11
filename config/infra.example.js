bd9a86c925e8ef21d58186943ace3402dc9dd774
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2026-09-11 17:58:23 +0200

    docs(config): remove optional parameters

--- config/infra.example.js
+++ config/infra.example.js
@@ -12,56 +12,44 @@ module.exports = {
         externalWebsocketURL: undefined,
         fileHost: undefined,
         // (Optional) httpServerId only useful for multi-server cases
-        httpServerId: '',
+        httpServerId: "",
     },
     // Configure the topology here. Add or remove nodes on each level
     // depending on your instance usage.
     // "host" and "port" correspond to the nodejs HTTP server of each node
-    // "url" can be set if your nodes are on different machines. They must
-    // be able to reach each other from this URL
+    // "url" is optional and can be set if your nodes are on different machines.
+    // They must be able to reach each other from this URL
     // "serverId" is optional and is only useful for multi-server cases
     "front": [
         {
-            url: "", // e.g. "https://node1.my-cryptpad-domain.net"
             host: "localhost",
             port: 3010, // Public http and websocket port
-            serverId: ''
         },
         {
-            url: "", // "https://node2.my-cryptpad-domain.net"
             host: "localhost",
             port: 3011,
-            serverId: ''
         }
     ],
     "core": [
         {
-            url: "",
             host: "localhost",
             port: 3020, // Internal websocket between all nodes
-            serverId: ''
         },
         {
-            url: "",
             host: "localhost",
             port: 3021,
-            serverId: ''
         }
     ],
     "storage": [
         {
-            url: "",
             host: "localhost",
             port: 3030, // Public port to serve "blob", "block", etc.
             wsPort: 3040, // Internal websocket between storage nodes
-            serverId: ''
         },
         {
-            url: "",
             host: "localhost",
             port: 3031,
             wsPort: 3041,
-            serverId: ''
         }
     ]
 };
