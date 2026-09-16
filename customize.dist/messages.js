3a979723f8eae1d141989a30455a41407d3a7b75
yflory <yann.flory@xwiki.com>
2026-09-16 12:48:34 +0200

    chore: remove hardcoded translation keys

--- customize.dist/messages.js
+++ customize.dist/messages.js
@@ -157,8 +157,6 @@ define(req, function(AppConfig, Default, Language) {
         }
     };
 
-    Messages.creation_expireValue = "Expiration time";
-    Messages.creation_expireUnit = "Expiration time unit"; // both used for the "number" and "time unit" dropdowns on pad creation page
     return Messages;
 
 });
