24c1d42336958c5b69b470b642e5a2fe7d0cc183
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2026-09-18 16:47:33 +0200

    chore: remove debug log

--- www/common/drive-ui.js
+++ www/common/drive-ui.js
@@ -5303,7 +5303,6 @@ define([
                     var auditorHash;
                     if (parsed.hash && parsed.type === "form") {
                         var formData = Hash.getFormData(null, parsed.hash, data.password);
-                        console.log(formData);
                         if (formData) {
                             auditorHash = formData.form_auditorHash;
                         }
