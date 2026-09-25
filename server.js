bd3ba701233962e94e26847c71a45889552754a5
David Benque <david@cryptpad.org>
2026-09-25 10:44:11 +0200

    chore: add REUSE licensing info for new files

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 XWiki CryptPad Team <contact@cryptpad.org> and contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 const Server = require("cryptpad-server");
 const { config, infra } = require('./lib/load-config');
 process.env.STANDALONE = false;
--- www/common/onlyoffice/trim-history.js
+++ www/common/onlyoffice/trim-history.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 XWiki CryptPad Team <contact@cryptpad.org> and contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 const factory = () => {
 
     const sortCpIndex = (hashes) => {
